e809ac1af11fdbea340e5afd79600402de9df56c
Shijir Tsogoo <stsogoo@vmware.com>
2016-11-09 12:23:10 -0800

    [ICONS] implemented a size attribute
    Signed-off-by: Shijir Tsogoo <stsogoo@vmware.com>

--- src/icons/define-clarity-icons.ts
+++ src/icons/define-clarity-icons.ts
@@ -18,7 +18,8 @@ function ClarityIcon() {
     return (parentConstructor as any).apply(this, arguments);
 }
 
-(ClarityIcon as any).observedAttributes = ["shape"];
+(ClarityIcon as any).observedAttributes = ["shape", "size"];
+
 ClarityIcon.prototype = Object.create(HTMLElement.prototype);
 ClarityIcon.prototype.constructor = ClarityIcon;
 let generateIcon = function (element: any, shape: string) {
@@ -28,12 +29,28 @@ let generateIcon = function (element: any, shape: string) {
         element.innerHTML = SVG_ICON_TEMPLATES[shape] || SVG_ICON_TEMPLATES["warning"];
     }
 };
+let setIconSize = function (element: any, size: string) {
+
+    if (!Number(size) || Number(size) < 0) {
+
+        element.style.width = null; // fallback to the original stylesheet value
+        element.style.height = null; // fallback to the original stylesheet value
+    } else {
+
+        element.style.width = size + "px";
+        element.style.height = size + "px";
+    }
+
+};
 ClarityIcon.prototype.connectedCallback = function () {
 
     let host = this as HTMLElement;
 
     if (host.hasAttribute("shape")) {
         generateIcon(host, host.getAttribute("shape"));
+    }
+    if (host.hasAttribute("size")) {
+        setIconSize(host, host.getAttribute("size"));
     }
 };
 ClarityIcon.prototype.attributeChangedCallback = function (attributeName: string, oldValue: string, newValue: string) {
@@ -43,6 +60,11 @@ ClarityIcon.prototype.attributeChangedCallback = function (attributeName: string
     if (attributeName === "shape") {
         generateIcon(host, newValue);
     }
+    if (attributeName === "size") {
+        setIconSize(host, newValue);
+    }
+
+
 };
 
 customElements.define("clr-icon", ClarityIcon);

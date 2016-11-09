2b40aa57d97bc38b1f610374fec3e12df925ab19
Aditya Bhandari <arb492@nyu.edu>
2016-11-08 20:03:02 -0800

    [UI] Breaking Alert Static Demos
    Breaking Alert Placement Demos
    
    Signed-off-by: Aditya Bhandari <arb492@nyu.edu>

--- src/clarity/alert/demo/static/alert-cards.ts
+++ src/clarity/alert/demo/static/alert-cards.ts
@@ -1,10 +1,10 @@
 import {Component} from "@angular/core";
 
 @Component({
-    selector: "clr-alert-demo-types",
+    selector: "clr-alert-demo-cards",
     // Note the .css extension here, not .scss. That's the best we can have at the moment.
     styleUrls: ["../alert.demo.css"],
-    templateUrl: "./alert-types.demo.html"
+    templateUrl: "./alert-cards.demo.html"
 })
-export class AlertTypesDemo {
+export class AlertCardsDemo {
 }
---
+++ src/clarity/alert/demo/static/alert-content-area.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "clr-alert-demo-content-area",
+    // Note the .css extension here, not .scss. That's the best we can have at the moment.
+    styleUrls: ["../alert.demo.css"],
+    templateUrl: "./alert-content-area.demo.html"
+})
+export class AlertContentAreaDemo {
+}

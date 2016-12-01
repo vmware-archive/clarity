d0a5d1d29bd3063fae00e2bdfc763b8f39ffe055
Aditya Bhandari <adityab@vmware.com>
2016-12-01 09:12:12 -0800

    [UI,WEB] Documentation Updates
    Documentation Updates:
    1. Change href=“javascript://“ to href=“…” in the code snippets
    2. Update Dropdown Clarity Icon to use a smaller icon
    3. Fix an issue with the header branding doc
    
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/clarity/dropdown/demo/dropdown.demo.ts
+++ src/clarity/dropdown/demo/dropdown.demo.ts
@@ -12,6 +12,7 @@ import {Component} from "@angular/core";
             <li><a [routerLink]="['./default']">Default Styles (Static)</a></li>
             <li><a [routerLink]="['./positioning']">Positioning with CSS Classnames (Static)</a></li>
             <li><a [routerLink]="['./fontawesome-toggle']">Icon Font Toggle (Static)</a></li>
+            <li><a [routerLink]="['./icon-toggle']">Clarity Icon Toggle (Static)</a></li>
             <li><a [routerLink]="['./buttonlink-toggle']">Button Link Toggle (Static)</a></li>
             <li><a [routerLink]="['./angular-positioning']">Positioning with Angular</a></li>
             <li><a [routerLink]="['./multi-click']">Stay Open After Click Event (Angular)</a></li>

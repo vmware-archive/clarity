1673d22d5c9882735381a00bf648afe89b71d482
Matt Hippely <mhippely@vmware.com>
2016-12-07 08:58:40 -0800

    [ui] Adds indeterminate state on checkboxes - closes #167
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/clarity/checkboxes/checkbox.ts
+++ src/clarity/checkboxes/checkbox.ts
@@ -14,7 +14,8 @@ let latestId = 0;
 @Component({
     selector: "clr-checkbox",
     template: `
-        <input type="checkbox" [id]="id" [name]="name" [checked]="checked" (change)="toggle()" (blur)="touch()">
+        <input type="checkbox" [id]="id" [name]="name" [checked]="checked" [indeterminate]="indeterminate" 
+               (change)="toggle()" (blur)="touch()">
         <label [attr.for]="id"><ng-content></ng-content></label>
     `,
     host: {
@@ -34,29 +35,54 @@ let latestId = 0;
 })
 export class Checkbox implements ControlValueAccessor {
     // If our host has an ID attribute, we use this instead of our index.
-    @Input("id") private _id: string = (latestId++).toString();
+    @Input("id")
+    private _id: string = (latestId++).toString();
+
     public get id() {
         return `clr-checkbox-${this._id}`;
     }
 
     // If our host has a name attribute, we apply it to the checkbox.
-    @Input("name") public name: string = null;
+    @Input("name")
+    public name: string = null;
 
     // Support for inline checkboxes, adds the necessary class to the host
     @Input("clrInline") public inline = false;
 
-    @Input("clrChecked") private _checked = false;
+    @Input("clrChecked")
+    private _checked = false;
+
     public get checked() {
         return this._checked;
     }
+
     public set checked(value: boolean) {
         if (value !== this._checked) {
             this._checked = value;
         }
     }
-    @Output("clrCheckedChange") public change = new EventEmitter<boolean>(false);
+
+    private _indeterminate: boolean = false;
+
+    public get indeterminate () {
+        return this._indeterminate;
+    }
+
+    @Input("clrIndeterminate")
+    public set indeterminate(value: boolean) {
+        this.checked = false;
+        this._indeterminate = value;
+        this.indeterminateChange.emit(this._indeterminate);
+    }
+
+    @Output("clrIndeterminateChange")
+    public indeterminateChange: EventEmitter<boolean> = new EventEmitter<boolean>(false);
+
+    @Output("clrCheckedChange")
+    public change = new EventEmitter<boolean>(false);
 
     public toggle() {
+        this._indeterminate = false;
         this.checked = !this.checked;
         this.onChangeCallback(this.checked);
         this.change.emit(this.checked);
--- src/clarity/checkboxes/demo/checkboxes.demo.ts
+++ src/clarity/checkboxes/demo/checkboxes.demo.ts
@@ -3,7 +3,7 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 @Component({
     selector: "clr-checkboxes-demo",
@@ -14,4 +14,11 @@ import {Component} from "@angular/core";
 
 export class CheckboxesDemo {
 
+    @Input()
+    indeterminateState: boolean = true;
+
+    onToggleIndeterminateState() {
+        event.preventDefault();
+        this.indeterminateState = !this.indeterminateState;
+    }
 }

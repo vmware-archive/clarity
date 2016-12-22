09d6830ac2e5ae34c617f6812d0e2ec186a878b5
smathis <smathis@vmware.com>
2016-12-22 09:38:10 -0600

    [web] fixing breakages/changes s with icon documentation
    Tested in:
    ✔︎ Chrome
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/demos/iconography/icon-variants.ts
+++ src/demos/iconography/icon-variants.ts
@@ -8,7 +8,7 @@ import {Component} from "@angular/core";
 @Component({
     selector: "clr-icon-variants-demo",
     // Note the .css extension here, not .scss. That's the best we can have at the moment.
-    styleUrls: ["./iconography.demo.css"],
+    styleUrls: ["./iconography.demo.scss"],
     templateUrl: "./icon-variants.demo.html"
 })
 export class IconVariantsDemo {

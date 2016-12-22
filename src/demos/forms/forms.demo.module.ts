7b84fac3b069ab9e394192a3f50dd1ea74dc1e56
smathis <smathis@vmware.com>
2016-12-22 11:43:34 -0600

    [web] adding reactive and template driven forms to new website
    Tested in:
    ✔︎ Chrome
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/demos/forms/forms.demo.module.ts
+++ src/demos/forms/forms.demo.module.ts
@@ -6,6 +6,8 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ClarityModule} from 'clarity-angular';
+import {FormsModule}   from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {FormFieldsDemo} from "./form-fields";
 import {FormTestDemo} from "./form-test";
@@ -13,24 +15,36 @@ import {FormValidationDemo} from "./form-validation";
 import {FormCompactDemo} from "./form-compact";
 import {FormGridDemo} from "./form-grid";
 
+import {TemplateDrivenFormsDemo} from "./template-driven-forms/template-driven-forms";
+import {ReactiveFormsDemo} from "./reactive-forms/reactive-forms";
+
+import {Example} from "./utils/example";
+
 @NgModule({
     imports: [
         CommonModule,
         ClarityModule.forChild(),
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         FormFieldsDemo,
         FormGridDemo,
         FormTestDemo,
         FormValidationDemo,
-        FormCompactDemo
+        FormCompactDemo,
+        TemplateDrivenFormsDemo,
+        ReactiveFormsDemo,
+        Example
     ],
     exports: [
         FormFieldsDemo,
         FormGridDemo,
         FormTestDemo,
         FormValidationDemo,
-        FormCompactDemo
+        FormCompactDemo,
+        TemplateDrivenFormsDemo,
+        ReactiveFormsDemo
     ]
 })
 export default class FormsDemoModule {

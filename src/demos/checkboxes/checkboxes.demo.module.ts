6054504612a650c4b5472d7b7ff8551b4eebeb21
Matt Hippely <mhippely@vmware.com>
2017-01-20 11:12:47 -0800

    [ng,web] Make the checkbox component public - Adds code and docs for the website for clr-checkbox, native checkbox usage - closes #319
    NOTE: Do not merge this to the website branch until after v0.8.4 is release to npm.
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/demos/checkboxes/checkboxes.demo.module.ts
+++ src/demos/checkboxes/checkboxes.demo.module.ts
@@ -5,17 +5,24 @@
  */
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 import {ClarityModule} from 'clarity-angular';
 
 import {CheckboxesDemo} from "./checkboxes.demo";
 
+import {Status} from "./data/status";
+
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         ClarityModule.forChild(),
     ],
     declarations: [
         CheckboxesDemo
+    ],
+    providers: [
+        Status
     ],
     exports: [
         CheckboxesDemo

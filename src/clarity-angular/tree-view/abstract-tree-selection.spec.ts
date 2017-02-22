4b24a05b44366f7757f2c74f434d97505394dd5c
Aditya Bhandari <adityab@vmware.com>
2017-02-22 12:21:14 -0800

    [NG] Add copyright messages to TreeView
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/clarity-angular/tree-view/abstract-tree-selection.spec.ts
+++ src/clarity-angular/tree-view/abstract-tree-selection.spec.ts
@@ -1,4 +1,8 @@
-
+/*
+ * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
 
 import {AbstractTreeSelection} from "./abstract-tree-selection";
 import {TreeSelectionService} from "./providers/treeSelection.service";

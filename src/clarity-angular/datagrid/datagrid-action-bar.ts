bd70fae508d19754fcb47e66471fe6348fd8c1c7
Jeeyun Lim <jeeyun.lim@gmail.com>
2017-03-31 14:24:43 -0700

    [ng] allow using datagrid action bar in non-selectable datagrids.
    resolves #662.
    
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- src/clarity-angular/datagrid/datagrid-action-bar.ts
+++ src/clarity-angular/datagrid/datagrid-action-bar.ts
@@ -4,19 +4,15 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import {Component} from "@angular/core";
-import {Selection} from "./providers/selection";
 
 @Component({
     selector: "clr-dg-action-bar",
     template: `
-        <ng-content *ngIf="selection.current && selection.current.length > 0"></ng-content>
+        <ng-content></ng-content>
     `,
     host: {
         "[class.datagrid-action-bar]": "true"
     }
 })
 export class DatagridActionBar {
-
-    constructor(public selection: Selection) {}
-
 }

678b5645a70f81ea7f365546d8c8bc06aef5acff
Jeeyun Lim <jeeyun.lim@gmail.com>
2017-03-31 14:24:43 -0700

    [ng] allow using datagrid action bar in non-selectable datagrids.
    resolves #662.
    
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- src/clarity-angular/datagrid/datagrid-action-bar.spec.ts
+++ src/clarity-angular/datagrid/datagrid-action-bar.spec.ts
@@ -4,7 +4,6 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import {Component} from "@angular/core";
-import {TestBed} from "@angular/core/testing";
 import {TestContext} from "./helpers.spec";
 import {DatagridActionBar} from "./datagrid-action-bar";
 import {Selection} from "./providers/selection";
@@ -16,23 +15,13 @@ import {Page} from "./providers/page";
 export default function(): void {
     describe("DatagridActionBar component", function() {
         let context: TestContext<DatagridActionBar, SimpleTest>;
-        let selectionProvider: Selection;
 
         beforeEach(function() {
             context = this.create(DatagridActionBar, SimpleTest, [Selection, Items, FiltersProvider, Sort, Page]);
-            selectionProvider = TestBed.get(Selection);
         });
 
-        it("projects content only if selection.current has items", function() {
-            expect(context.clarityElement.textContent.trim()).toMatch("");
-
-            selectionProvider.current = [1];
-            context.detectChanges();
+        it("projects content", function() {
             expect(context.clarityElement.textContent.trim()).toMatch("Hello world");
-
-            selectionProvider.current = [];
-            context.detectChanges();
-            expect(context.clarityElement.textContent.trim()).toMatch("");
         });
     });
 }

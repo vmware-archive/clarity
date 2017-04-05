3be59313b6462996879a95de51554c13d9e61efb
Matt Hippely <hippely@gmail.com>
2017-04-05 15:53:34 -0700

    Added release notes for 0.8.14.
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/releases/release-templates-stub.ts
+++ src/releases/release-templates-stub.ts
@@ -5,6 +5,7 @@
 export const TEMPLATES = {
   '0.9.0-alpha.2': require('./0.9/0.9.0-alpha.2.html'),
 
+  '0.8.14': require('./0.8/0.8.14.html'),
   '0.8.13': require('./0.8/0.8.13.html'),
   '0.8.12': require('./0.8/0.8.12.html'),
   '0.8.11': require('./0.8/0.8.11.html'),

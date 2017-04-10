13dbc476370cb3e4d7b32e13cb1955052f66e39a
smathis <smathis@vmware.com>
2017-04-10 15:54:12 -0500

    [WEB] 0.9.0 release notes - MERGE AFTER 0.9.0
    Updating 0.9.0-alpha release notes for impending 0.9.0 release.
    
    Doing this as a branch on clarity so Jeeyun can edit if needed.
    
    Tested in:
    ✔︎ Chrome
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/releases/release-templates-stub.ts
+++ src/releases/release-templates-stub.ts
@@ -3,7 +3,7 @@
  * It works fine with webpack, but angular-cli breaks them. See https://github.com/angular/angular-cli/issues/3306
  */
 export const TEMPLATES = {
-  '0.9.0-alpha.2': require('./0.9/0.9.0-alpha.2.html'),
+  '0.9.0': require('./0.9/0.9.0.html'),
 
   '0.8.14': require('./0.8/0.8.14.html'),
   '0.8.13': require('./0.8/0.8.13.html'),

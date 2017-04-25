ada0a09750a2f8ee1339add6c6e4ed24f972adc8
Scott Mathis <mathisscott@users.noreply.github.com>
2017-04-25 10:50:43 -0500

    [WEB] new color palette! (#773)
    * [WEB] new color palette!
    
    Closes #768
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>
    
    * [WEB] made #FAFAFA big like rest of base/core palette
    
    • per @lil-kim ’s comment
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/demos/color/color.demo.module.ts
+++ src/demos/color/color.demo.module.ts
@@ -5,30 +5,24 @@
  */
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {ClarityModule} from 'clarity-angular';
+import {ClarityModule} from "clarity-angular";
 
-import {ColorPaletteBaseDemo} from "./color-palette-base";
-import {ColorPaletteStoplightDemo} from "./color-palette-stoplight";
-import {ColorPaletteHighlightDemo} from "./color-palette-highlight";
 import {ColorLuminanceDemo} from "./color-luminance";
 import {ColorContrastDemo} from "./color-contrast";
+import { ColorPalette } from "./color-palette";
 
 @NgModule({
     imports: [
         CommonModule,
-        ClarityModule.forChild(),
+        ClarityModule.forChild()
     ],
     declarations: [
-        ColorPaletteBaseDemo,
-        ColorPaletteStoplightDemo,
-        ColorPaletteHighlightDemo,
+        ColorPalette,
         ColorLuminanceDemo,
         ColorContrastDemo
     ],
     exports: [
-        ColorPaletteBaseDemo,
-        ColorPaletteStoplightDemo,
-        ColorPaletteHighlightDemo,
+        ColorPalette,
         ColorLuminanceDemo,
         ColorContrastDemo
     ]

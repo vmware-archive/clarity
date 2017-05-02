1b588d7c527177aa4283306bd7cbd553cd91df95
Aditya Bhandari <adityab@vmware.com>
2017-05-02 13:41:00 -0700

    Code Snippet Component
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -3,17 +3,26 @@ import {NgModule} from "@angular/core";
 import {SketchTemplateLinkDirective} from "./sketch-template-link.directive";
 import {HashListener} from "./hash-listener.directive";
 import {ScrollSpy} from "./scrollspy.directive";
+import {ClarityModule} from "clarity-angular";
+import {CodeSnippet} from "./code-snippet";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
+    imports: [
+        CommonModule,
+        ClarityModule.forChild()
+    ],
     declarations: [
         SketchTemplateLinkDirective,
         HashListener,
-        ScrollSpy
+        ScrollSpy,
+        CodeSnippet
     ],
     exports: [
         SketchTemplateLinkDirective,
         HashListener,
-        ScrollSpy
+        ScrollSpy,
+        CodeSnippet
     ]
 })
 export class UtilsModule {

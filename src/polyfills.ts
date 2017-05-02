ab9c5bd57236d28625ca8f612af6a6315fae6ebf
Aditya Bhandari <adityab@vmware.com>
2017-05-02 08:55:53 -0700

    IE 11 Test
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,23 +1,25 @@
 // This file includes polyfills needed by Angular and is loaded before
 // the app. You can add your own extra polyfills to this file.
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/es6/reflect';
+import "core-js/es6/symbol";
+import "core-js/es6/object";
+import "core-js/es6/function";
+import "core-js/es6/parse-int";
+import "core-js/es6/parse-float";
+import "core-js/es6/number";
+import "core-js/es6/math";
+import "core-js/es6/string";
+import "core-js/es6/date";
+import "core-js/es6/array";
+import "core-js/es6/regexp";
+import "core-js/es6/map";
+import "core-js/es6/set";
+import "core-js/es6/reflect";
+import "core-js/client/shim";
 
-import 'core-js/es7/reflect';
+import "core-js/es7/reflect";
 
-// import 'intl';
-// import 'intl/locale-data/jsonp/en';
+import "intl";
+import "intl/locale-data/jsonp/en";
 
-import 'zone.js/dist/zone';
+import "zone.js/dist/zone";
+import "reflect-metadata";

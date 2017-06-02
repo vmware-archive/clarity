861ac07c12c81cd6a996bd7cdfc64a4c09ba0824
Aditya Bhandari <adityab@vmware.com>
2017-06-02 07:27:05 -0700

    [NG] Add disabled, type & name attr to clr-button
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/clarity-angular/button/button-group/button.ts
+++ src/clarity-angular/button/button-group/button.ts
@@ -12,7 +12,12 @@ import {ButtonInGroupService} from "../providers/buttonInGroup.service";
     selector: "clr-button",
     template: `
         <ng-template #buttonProjectedRef>
-            <button [class]="classNames" (click)="emitClick()">
+            <button 
+                [class]="classNames" 
+                (click)="emitClick()"
+                [attr.type]="type"
+                [attr.name]="name"
+                [attr.disabled]="disabled">
                 <ng-content></ng-content>
             </button>
         </ng-template>
@@ -53,8 +58,53 @@ export class Button {
 
     @Input("class")
     set classNames(value: string) {
-        if (value) {
-            this._classNames = value;
+        if (typeof value === "string") {
+            let classNames: string[] = value.split(" ");
+            if (classNames.indexOf("btn") === -1) {
+                classNames.push("btn");
+            }
+            this._classNames = classNames.join(" ");
+        }
+    }
+
+    private _name: string = null;
+
+    get name(): string {
+        return this._name;
+    }
+
+    @Input("name")
+    set name(value: string) {
+        if (typeof value === "string") {
+            this._name = value;
+        }
+    }
+
+    private _type: string = null;
+
+    get type(): string {
+        return this._type;
+    }
+
+    @Input("type")
+    set type(value: string) {
+        if (typeof value === "string") {
+            this._type = value;
+        }
+    }
+
+    private _disabled: any = null;
+
+    get disabled(): any {
+        return this._disabled;
+    }
+
+    @Input("disabled")
+    set disabled(value: any) {
+        if (value !== null && value !== false) {
+            this._disabled = "";
+        } else {
+            this._disabled = null;
         }
     }
 

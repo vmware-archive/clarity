1a43abaa22cb150cb029dcec9f85bb4090f56695
Matt Hippely <mhippely@vmware.com>
2017-06-20 10:56:05 -0700

    [NG] Adds custom trigger to signposts - Adds gemini testing for signpost - updates signpost tests for new position values - adds test for custom signpost trigger - closes #1032 - Changes Signpost template to use new Popover attribute directive instead of old structural directive
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

---
+++ gemini/tests/set2/signposts.js
@@ -0,0 +1,48 @@
+var WAIT_TIME = 5000;
+var WAIT_LOAD_TIME = 1000;
+
+gemini.suite('signposts', (child) => {
+    child.setUrl('/signposts')
+        .before((actions) => {
+            actions.waitForElementToShow('.content-area', WAIT_TIME);
+            actions.wait(WAIT_LOAD_TIME);
+        })
+        .setCaptureElements('.content-area')
+        .capture('default')
+        .capture('top-left', function(actions, find) {
+            actions.click(find('#topLeftTrigger', WAIT_TIME));
+        })
+        .capture('top-middle', function(actions, find) {
+            actions.click(find('#topMiddleTrigger', WAIT_TIME));
+        })
+        .capture('top-right', function(actions, find) {
+            actions.click(find('#topRightTrigger', WAIT_TIME));
+        })
+        .capture('right-top', function(actions, find) {
+            actions.click(find('#rightTopTrigger', WAIT_TIME));
+        })
+        .capture('right-middle', function(actions, find) {
+            actions.click(find('#rightMiddleTrigger', WAIT_TIME));
+        })
+        .capture('right-bottom', function(actions, find) {
+            actions.click(find('#rightBottomTrigger', WAIT_TIME));
+        })
+        .capture('bottom-left', function(actions, find) {
+            actions.click(find('#bottomLeftTrigger', WAIT_TIME));
+        })
+        .capture('bottom-middle', function(actions, find) {
+            actions.click(find('#bottomMiddleTrigger', WAIT_TIME));
+        })
+        .capture('bottom-right', function(actions, find) {
+            actions.click(find('#bottomRightTrigger', WAIT_TIME));
+        })
+        .capture('left-top', function(actions, find) {
+            actions.click(find('#leftTopTrigger', WAIT_TIME));
+        })
+        .capture('left-middle', function(actions, find) {
+            actions.click(find('#leftMiddleTrigger', WAIT_TIME));
+        })
+        .capture('left-bottom', function(actions, find) {
+            actions.click(find('#leftBottomTrigger', WAIT_TIME));
+        })
+});

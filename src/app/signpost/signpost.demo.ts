1875ac7d1cfa9475756b4ff394ea82a1359bae8e
Matt Hippely <mhippely@vmware.com>
2017-06-30 10:43:58 -0700

    [CORE] Adds some wait time and visibility checking to Signpost gemini test suite.
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/app/signpost/signpost.demo.ts
+++ src/app/signpost/signpost.demo.ts
@@ -18,53 +18,53 @@ export class SignpostDemo {
     openState: boolean = false;
     positions = [
         {
-            description: "Top left",
-            position: "top-left",
-            id: "topLeftTrigger"
-        }, {
-            description: "Top middle",
-            position: "top-middle",
-            id: "topMiddleTrigger"
-        }, {
-            description: "Top right",
-            position: "top-right",
-            id: "topRightTrigger"
-        }, {
-            description: "Right top",
-            position: "right-top",
-            id: "rightTopTrigger"
-        }, {
-            description: "Right middle",
-            position: "right-middle",
-            id: "rightMiddleTrigger"
-        }, {
             description: "Right bottom",
             position: "right-bottom",
             id: "rightBottomTrigger"
-        }, {
-            description: "Bottom left",
-            position: "bottom-left",
-            id: "bottomLeftTrigger"
         }, {
             description: "Bottom middle",
             position: "bottom-middle",
             id: "bottomMiddleTrigger"
         }, {
-            description: "Bottom right",
-            position: "bottom-right",
-            id: "bottomRightTrigger"
+            description: "Left bottom",
+            position: "left-bottom",
+            id: "leftBottomTrigger"
         }, {
-            description: "Left top",
-            position: "left-top",
-            id: "leftTopTrigger"
+            description: "Right middle",
+            position: "right-middle",
+            id: "rightMiddleTrigger"
         }, {
             description: "Left middle",
             position: "left-middle",
             id: "leftMiddleTrigger"
         }, {
-            description: "Left bottom",
-            position: "left-bottom",
-            id: "leftBottomTrigger"
+            description: "Left top",
+            position: "left-top",
+            id: "leftTopTrigger"
+        }, {
+            description: "Right top",
+            position: "right-top",
+            id: "rightTopTrigger"
+        }, {
+            description: "Bottom right",
+            position: "bottom-right",
+            id: "bottomRightTrigger"
+        }, {
+            description: "Bottom left",
+            position: "bottom-left",
+            id: "bottomLeftTrigger"
+        }, {
+            description: "Top right",
+            position: "top-right",
+            id: "topRightTrigger"
+        }, {
+            description: "Top middle",
+            position: "top-middle",
+            id: "topMiddleTrigger"
+        },{
+            description: "Top left",
+            position: "top-left",
+            id: "topLeftTrigger"
         }
     ];
 }

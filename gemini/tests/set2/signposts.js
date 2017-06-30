4e4f22927b11b51ef0f6fce541b32de88b784b88
Matt Hippely <mhippely@vmware.com>
2017-06-30 10:43:58 -0700

    [CORE] Adds some wait time and visibility checking to Signpost gemini test suite.
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- gemini/tests/set2/signposts.js
+++ gemini/tests/set2/signposts.js
@@ -1,6 +1,18 @@
 var WAIT_TIME = 5000;
 var WAIT_LOAD_TIME = 1000;
 
+// gemini.suite('dynamic-show', (child) => {
+//     child.setUrl('/modal/dynamic-show')
+//         .before((actions, find) => {
+//             actions.waitForElementToShow('.btn', WAIT_TIME);
+//             actions.click(find('.btn'));
+//             actions.waitForElementToShow('.modal-content', WAIT_TIME);
+//             actions.wait(WAIT_TIME); // wait for modal to fully appear
+//         })
+//         .setCaptureElements('.modal-content')
+//         .capture('default');
+// });
+
 gemini.suite('signposts', (child) => {
     child.setUrl('/signposts')
         .before((actions) => {
@@ -11,38 +23,62 @@ gemini.suite('signposts', (child) => {
         .capture('default')
         .capture('top-left', function(actions, find) {
             actions.click(find('#topLeftTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('top-middle', function(actions, find) {
             actions.click(find('#topMiddleTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('top-right', function(actions, find) {
             actions.click(find('#topRightTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('right-top', function(actions, find) {
             actions.click(find('#rightTopTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('right-middle', function(actions, find) {
             actions.click(find('#rightMiddleTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('right-bottom', function(actions, find) {
             actions.click(find('#rightBottomTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('bottom-left', function(actions, find) {
             actions.click(find('#bottomLeftTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('bottom-middle', function(actions, find) {
             actions.click(find('#bottomMiddleTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('bottom-right', function(actions, find) {
             actions.click(find('#bottomRightTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('left-top', function(actions, find) {
             actions.click(find('#leftTopTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('left-middle', function(actions, find) {
             actions.click(find('#leftMiddleTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
         .capture('left-bottom', function(actions, find) {
             actions.click(find('#leftBottomTrigger', WAIT_TIME));
+            actions.waitForElementToShow('.signpost-content', WAIT_TIME);
+            actions.wait(WAIT_TIME);
         })
 });

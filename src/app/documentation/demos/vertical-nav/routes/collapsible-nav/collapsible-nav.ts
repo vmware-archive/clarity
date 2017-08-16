5bfd783e34e5c67e6ccb58d02446e221946e8fa5
Aditya Bhandari <adityab@vmware.com>
2017-08-16 13:44:16 -0400

    [WEB] Vertical Nav
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

---
+++ src/app/documentation/demos/vertical-nav/routes/collapsible-nav/collapsible-nav.ts
@@ -0,0 +1,47 @@
+/*
+* Copyright (c) 2016 - 2017 VMware, Inc. All Rights Reserved.
+* This software is released under MIT license.
+* The full license information can be found in LICENSE in the root directory of this project.
+*/
+import {Component} from "@angular/core";
+
+const HTML_EXAMPLE = `
+<clr-vertical-nav [clrVerticalNavCollapsible]="collapsible" [(clrVerticalNavCollapsed)]="collapsed">
+    <a clrVerticalNavLink routerLink="./normal" routerLinkActive="active">
+        <clr-icon clrVerticalNavIcon shape="user"></clr-icon>
+        Normal
+    </a>
+    <a clrVerticalNavLink routerLink="./electric" routerLinkActive="active">
+        <clr-icon clrVerticalNavIcon shape="bolt"></clr-icon>
+        Electric
+    </a>
+    <a clrVerticalNavLink routerLink="./poison" routerLinkActive="active">
+        <clr-icon clrVerticalNavIcon shape="sad-face"></clr-icon>
+        Poison
+    </a>
+    <a clrVerticalNavLink routerLink="./grass" routerLinkActive="active">
+        <clr-icon clrVerticalNavIcon shape="bug"></clr-icon>
+        Grass
+    </a>
+    <a clrVerticalNavLink routerLink="./fighting" routerLinkActive="active">
+        <clr-icon clrVerticalNavIcon shape="shield"></clr-icon>
+        Fighting
+    </a>
+    <a clrVerticalNavLink routerLink="./credit" routerLinkActive="active">
+        <clr-icon clrVerticalNavIcon shape="certificate"></clr-icon>
+        Credit
+    </a>
+</clr-vertical-nav>
+`;
+
+@Component({
+    selector: "clr-vertical-nav-collapsible-demo",
+    templateUrl: "./collapsible-nav.html",
+    styleUrls: ["../../vertical-nav.demo.scss"]
+})
+export class CollapsibleNavDemo {
+    htmlExample = HTML_EXAMPLE;
+
+    collapsible: boolean = true;
+    collapsed: boolean = false;
+}
---
+++ src/app/documentation/demos/vertical-nav/routes/pokemons/pidgey.ts
@@ -0,0 +1,21 @@
+/*
+* Copyright (c) 2016 - 2017 VMware, Inc. All Rights Reserved.
+* This software is released under MIT license.
+* The full license information can be found in LICENSE in the root directory of this project.
+*/
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "clr-pidgey-demo",
+    template: `
+        <h4 class="demo-title">Pidgey</h4>
+        <p>
+            Pidgey has an extremely sharp sense of direction. 
+            It is capable of unerringly returning home to its nest, 
+            however far it may be removed from its familiar surroundings.
+        </p>
+    `,
+    styleUrls: ["../../vertical-nav.demo.scss"]
+})
+export class PidgeyDemo {
+}

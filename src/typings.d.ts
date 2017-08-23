d655202b7b82a1e20717b31c9b033230144069ae
Shijir Tsogoo <stsogoo@vmware.com>
2017-08-23 12:10:52 -0700

    [ICONS] remove dompurify from Clarity Icons
    Signed-off-by: Shijir Tsogoo <stsogoo@vmware.com>

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,8 @@
 /*
  * Copyright (c) 2016 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
- * The full license information can be found in LICENSE in the root directory of this project.
+ * The full license information can be found in LICENSE in the root directory of
+ * this project.
  */
 /**
  * We declare the module variable provided by the CommonJS module format,
@@ -10,11 +11,10 @@
 declare var module: Module;
 
 declare interface Module {
-    // So far, we"re only using the id property.
-    id: string;
+  // So far, we"re only using the id property.
+  id: string;
 }
 
-interface Window { 
-    ClarityIcons: any;
-    DOMPurify: any;
+interface Window {
+  ClarityIcons: any;
 }

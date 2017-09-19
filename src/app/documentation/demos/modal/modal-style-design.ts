18f607a92bd07a41b33b9be4acc6b20a61b3c021
stsogoo <stsogoo@vmware.com>
2017-09-18 19:19:51 -0700

    [WEB] disable focus trap in static modal and wizard demos
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/app/documentation/demos/modal/modal-style-design.ts
+++ src/app/documentation/demos/modal/modal-style-design.ts
@@ -3,14 +3,18 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
+import { FocusTrapTracker } from "clarity-angular/utils/focus-trap/focus-trap-tracker.service";
+import { DisableFocusTrap } from "../../utils/disable-focus-trap";
+
 
 @Component({
     selector: "clr-modal-style-design-demo",
     templateUrl: "./modal-style-design.demo.html",
     host: {
         "[class.in-place-takeover]": "true"
-    }
+    },
+    providers: [{provide: FocusTrapTracker, useClass: DisableFocusTrap}]
 })
 export class ModalStyleDesignDemo {
     public basic: boolean = true;

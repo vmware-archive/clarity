36a1beaafa2c50980fd45d994a2453efef697cb6
Eudes Petonnet-Vincent <epetonnetvince@vmware.com>
2017-09-18 20:15:09 -0400

    [NG] Internal structural directive for virtual scrolling
    CAUTION: This directive is for internal use only, we do not support its use outside of our own components.
    
    See demos for code examples on how to use it.
    It's still fairly crude and missing many customizations, but we can grow it organically as we go.
    
    Whenever we are making a change to this, we'll need to check for potential performance issues and
    make sure it runs correctly on all browsers.
    
    Signed-off-by: Eudes Petonnet-Vincent <epetonnetvince@vmware.com>

---
+++ src/clarity-angular/utils/virtual-scroll/all.spec.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016-2017 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+/**
+ * This file is just my OCD coding in my place.
+ *
+ * The goal is to have the tests properly grouped in the reporter, instead of having them all
+ * over the place because we load them asynchronously.
+ */
+import DomHelperSpecs from "./dom-helpers.spec";
+import VirtualForOfSpecs from "./virtual-for-of.spec";
+
+
+describe("Virtual Scroll", function() {
+    DomHelperSpecs();
+    VirtualForOfSpecs();
+});

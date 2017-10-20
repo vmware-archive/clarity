a155b7af9ebdbce9ba79813ba991df9c7bc30061
Aditya Bhandari <adityab@vmware.com>
2017-10-19 21:09:11 -0400

    [NG] ng container bug fix for popovers
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/clarity-angular/popover/tooltip/tooltip.ts
+++ src/clarity-angular/popover/tooltip/tooltip.ts
@@ -3,8 +3,9 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import {Component} from "@angular/core";
+import {Component, ElementRef, InjectionToken} from "@angular/core";
 import {IfOpenService} from "../../utils/conditional/if-open.service";
+import {POPOVER_HOST_ANCHOR} from "../common/popover-host-anchor.token";
 
 @Component({
     selector: "clr-tooltip",
@@ -14,6 +15,6 @@ import {IfOpenService} from "../../utils/conditional/if-open.service";
     host: {
         "[class.tooltip]": "true",
     },
-    providers: [IfOpenService]
+    providers: [IfOpenService, {provide: POPOVER_HOST_ANCHOR, useExisting: ElementRef}]
 })
 export class Tooltip {}

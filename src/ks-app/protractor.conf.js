1e5d41d206d2a259e7c57a665f746ce1c62d5baf
Matt Hippely <mhippely@vmware.com>
2018-01-02 15:41:58 -0800

    [CORE] Update Copyright Dates for 2018
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/ks-app/protractor.conf.js
+++ src/ks-app/protractor.conf.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 

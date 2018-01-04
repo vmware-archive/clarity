fbb9d9494bc376d838c91511cfdc34c3a9e7c7b7
Matt Hippely <mhippely@vmware.com>
2018-01-04 15:12:26 -0800

    [CORE] Update Copyright (2nd pass)
    - Removes `loud` scss comments from previous Copyright update
    - Removes multiline scss comments that were poluting the generated css
    - Adds missing copyright to script files outside of the `src/` folder
    - Updates several `src/` files that were missed in the first pass
    - Generated CSS Filesize
         - clarity-ui-dark.css = 428KB
         - clarity-ui-dark.min.css = 367KB
         - clarity-ui.css = 463KB
         - clarity-ui.min.css = 402KB
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- tests/tests.entry.ts
+++ tests/tests.entry.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
 import "core-js";
 import "core-js/es7/reflect";
 import "zone.js/dist/zone";
@@ -8,10 +14,12 @@ import "zone.js/dist/sync-test";
 import "zone.js/dist/proxy";
 import "zone.js/dist/jasmine-patch";
 
+/* tslint:disable no-var-requires no-require-imports */
 const browserTesting = require("@angular/platform-browser-dynamic/testing");
 const coreTesting = require("@angular/core/testing");
 // include every .spec.ts file inside src, except for those in ks-app folder
 const context = (require as any).context("../src/", true, /^((?![\\/]ks-app[\\/]).)*\.spec\.ts$/);
+/* tslint:enable no-var-requires no-require-imports*/
 
 Error.stackTraceLimit = Infinity;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;

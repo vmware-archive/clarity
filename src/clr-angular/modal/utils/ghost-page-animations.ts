b1fdf7039a8ba58d6dc592d004d94094184a51d4
Jeeyun Lim <jeeyun.lim@gmail.com>
2018-05-02 15:56:57 -0700

    [core] deprecate wizard ghost pages (#2151)
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- src/clr-angular/modal/utils/ghost-page-animations.ts
+++ src/clr-angular/modal/utils/ghost-page-animations.ts
@@ -3,6 +3,10 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
+
+/*
+ * @deprecated since 0.12
+ */
 export const GHOST_PAGE_ANIMATION = {
     STATES: {NO_PAGES: "inactive", ALL_PAGES: "ready", NEXT_TO_LAST_PAGE: "penultimateGhost", LAST_PAGE: "lastGhost"},
     TRANSITIONS: {IN: "100ms ease-out", OUT: "100ms ease-in"}

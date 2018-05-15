fc7cc0118ae84b1ed92262302e71f9760a48803d
Matt Hippely <mhippely@vmware.com>
2018-05-15 10:24:38 -0700

    feature: [CORE] add prettier, precise-commits and husky - prettier is opinionated formatting and configured in prettier.config.js - precise-commits allows formatting only for incremental changes that get commited - husky wires up a precommit git hook that runs for added files before commiting a changeset - Travis is configured to check formatting only on PR's, it will fail the job is the prettier formating check fails
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5', // other options `es5` or `all`
+    bracketSpacing: true,
+    arrowParens: 'avoid',  // other option 'always'
+    parser: 'typescript',
+};

2f0e81b0bb905e0c48298405bb41b1b154989c9e
Matt Hippely <mhippely@vmware.com>
2018-05-22 15:16:50 -0700

    Starting fresh
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- prettier.config.js
+++ prettier.config.js
@@ -5,13 +5,12 @@
  */
 
 module.exports = {
-    printWidth: 120,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'es5', // other options `es5` or `all`
-    bracketSpacing: true,
-    arrowParens: 'avoid',  // other option 'always'
-    parser: 'typescript',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5', // other options `es5` or `all`
+  bracketSpacing: true,
+  arrowParens: 'avoid', // other option 'always'
 };

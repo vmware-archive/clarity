55fd2d09aad1211626146827dc2abf63bb4f51ed
Jeremy Wilken <gnomation@gnomeontherun.com>
2018-06-08 20:06:41 -0500

    [CORE] setup daily cron job in main clarity repo
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

---
+++ scripts/travis.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/*
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+const shell = require('shelljs');
+
+const AngularPackages = [
+  'https://github.com/angular/animations-builds',
+  'https://github.com/angular/common-builds',
+  'https://github.com/angular/compiler-builds',
+  'https://github.com/angular/cli-builds',
+  'https://github.com/angular/compiler-cli-builds',
+  'https://github.com/angular/core-builds',
+  'https://github.com/angular/forms-builds',
+  'https://github.com/angular/platform-browser-builds',
+  'https://github.com/angular/platform-browser-dynamic-builds',
+  'https://github.com/angular/router-builds',
+  'https://github.com/angular/language-service-builds',
+  'https://github.com/angular/angular-devkit-build-angular-builds',
+  'https://github.com/angular/angular-devkit-build-ng-packagr-builds',
+  'https://github.com/angular/angular-devkit-build-optimizer-builds',
+  'https://github.com/angular/angular-devkit-core-builds',
+];
+
+// If we are running the cron job, assume it is for daily testing of latest builds on master
+if (
+  process.env.TRAVIS_EVENT_TYPE &&
+  process.env.TRAVIS_EVENT_TYPE === 'cron' &&
+  process.env.TRAVIS_BRANCH === 'master'
+) {
+  shell.exec(`npm install ${AngularPackages.join(' ')} --no-save`);
+  shell.exec('npm run $TEST_SUITE');
+} else {
+  shell.exec('npm run $TEST_SUITE');
+}

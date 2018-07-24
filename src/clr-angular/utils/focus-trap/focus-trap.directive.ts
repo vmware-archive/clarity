411a575b6e63f9c599f99ec0f66a157d2990faa3
stsogoo <stsogoo@vmware.com>
2018-07-24 15:34:22 -0700

    [NG] add elements that rebound focus back to trapper
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/clr-angular/utils/focus-trap/focus-trap.directive.ts
+++ src/clr-angular/utils/focus-trap/focus-trap.directive.ts
@@ -13,6 +13,7 @@ import {
   Injector,
   OnDestroy,
   PLATFORM_ID,
+  Renderer2,
 } from '@angular/core';
 
 import { FocusTrapTracker } from './focus-trap-tracker.service';
@@ -22,10 +23,14 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
   private previousActiveElement: any;
   private document: Document;
 
+  private topReboundEl: any;
+  private bottomReboundEl: any;
+
   constructor(
     private el: ElementRef,
     private injector: Injector,
     private focusTrapsTracker: FocusTrapTracker,
+    private renderer: Renderer2,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
     this.document = this.injector.get(DOCUMENT);
@@ -36,15 +41,48 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
   onFocusIn(event: any) {
     const nativeElement: HTMLElement = this.el.nativeElement;
 
-    if (this.focusTrapsTracker.current === this && !nativeElement.contains(event.target)) {
+    if (this.focusTrapsTracker.current === this && event.target && !nativeElement.contains(event.target)) {
       nativeElement.focus();
     }
   }
 
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.previousActiveElement = <HTMLElement>this.document.activeElement;
-      this.el.nativeElement.setAttribute('tabindex', '0');
+  private createFocusableOffScreenEl(nodeName: string, optionalClass?: string): any {
+    const offScreenSpan = this.renderer.createElement(nodeName);
+    this.renderer.setAttribute(offScreenSpan, 'tabindex', '0');
+    this.renderer.addClass(offScreenSpan, 'offscreen-focus-rebounder');
+
+    if (optionalClass) {
+      this.renderer.addClass(offScreenSpan, optionalClass);
+    }
+
+    return offScreenSpan;
+  }
+
+  private addReboundEls() {
+    // We will add these focus rebounding elements only in the following conditions:
+    // 1. It should be running inside browser platform as it accesses document.body element
+    // 2. We should add them more than once. Hence, we are counting a number of focus trappers
+    //    and only add on the first focus trapper.
+
+    if (this.focusTrapsTracker.nbFocusTrappers === 1) {
+      this.topReboundEl = this.createFocusableOffScreenEl('span', 'before-focus-trap');
+      this.bottomReboundEl = this.createFocusableOffScreenEl('span', 'after-focus-trap');
+
+      // Add reboundBeforeTrapEl to the document body as the first child
+      const bodyFirstChild = this.document.body.firstChild;
+      if (bodyFirstChild) {
+        this.renderer.insertBefore(this.document.body, this.topReboundEl, bodyFirstChild);
+      }
+
+      // Add reboundAfterTrapEl to the document body as the last child
+      this.renderer.appendChild(this.document.body, this.bottomReboundEl);
+    }
+  }
+
+  private removeReboundEls() {
+    if (this.focusTrapsTracker.nbFocusTrappers === 1 && this.topReboundEl && this.bottomReboundEl) {
+      this.renderer.removeChild(this.document.body, this.topReboundEl);
+      this.renderer.removeChild(this.document.body, this.bottomReboundEl);
     }
   }
 
@@ -54,7 +92,20 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
     }
   }
 
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.previousActiveElement = <HTMLElement>this.document.activeElement;
+      this.renderer.setAttribute(this.el.nativeElement, 'tabindex', '0');
+
+      this.addReboundEls();
+    }
+  }
+
   ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.removeReboundEls();
+    }
+
     this.setPreviousFocus();
     this.focusTrapsTracker.activatePreviousTrapper();
   }

9f970ce626a175d25ed1c956e4e64eb722bc71b9
stsogoo <stsogoo@vmware.com>
2018-07-31 16:52:34 -0700

    update in the directive
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/clr-angular/utils/focus-trap/focus-trap.directive.ts
+++ src/clr-angular/utils/focus-trap/focus-trap.directive.ts
@@ -35,6 +35,8 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
   ) {
     this.document = this.injector.get(DOCUMENT);
     this.focusTrapsTracker.current = this;
+
+    this.renderer.setAttribute(this.el.nativeElement, 'tabindex', '0');
   }
 
   @HostListener('document:focusin', ['$event'])
@@ -46,14 +48,10 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
     }
   }
 
-  private createFocusableOffScreenEl(nodeName: string, optionalClass?: string): any {
-    const offScreenSpan = this.renderer.createElement(nodeName);
+  private createFocusableOffScreenEl(): any {
+    const offScreenSpan = this.renderer.createElement('span');
     this.renderer.setAttribute(offScreenSpan, 'tabindex', '0');
     this.renderer.addClass(offScreenSpan, 'offscreen-focus-rebounder');
-
-    if (optionalClass) {
-      this.renderer.addClass(offScreenSpan, optionalClass);
-    }
 
     return offScreenSpan;
   }
@@ -61,12 +59,12 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
   private addReboundEls() {
     // We will add these focus rebounding elements only in the following conditions:
     // 1. It should be running inside browser platform as it accesses document.body element
-    // 2. We should add them more than once. Hence, we are counting a number of focus trappers
+    // 2. We should NOT add them more than once. Hence, we are counting a number of focus trappers
     //    and only add on the first focus trapper.
 
-    if (this.focusTrapsTracker.nbFocusTrappers === 1) {
-      this.topReboundEl = this.createFocusableOffScreenEl('span', 'before-focus-trap');
-      this.bottomReboundEl = this.createFocusableOffScreenEl('span', 'after-focus-trap');
+    if (isPlatformBrowser(this.platformId) && this.focusTrapsTracker.nbFocusTrappers === 1) {
+      this.topReboundEl = this.createFocusableOffScreenEl();
+      this.bottomReboundEl = this.createFocusableOffScreenEl();
 
       // Add reboundBeforeTrapEl to the document body as the first child
       const bodyFirstChild = this.document.body.firstChild;
@@ -80,7 +78,12 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
   }
 
   private removeReboundEls() {
-    if (this.focusTrapsTracker.nbFocusTrappers === 1 && this.topReboundEl && this.bottomReboundEl) {
+    if (
+      isPlatformBrowser(this.platformId) &&
+      this.focusTrapsTracker.nbFocusTrappers === 1 &&
+      this.topReboundEl &&
+      this.bottomReboundEl
+    ) {
       this.renderer.removeChild(this.document.body, this.topReboundEl);
       this.renderer.removeChild(this.document.body, this.bottomReboundEl);
     }
@@ -95,17 +98,13 @@ export class FocusTrapDirective implements AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       this.previousActiveElement = <HTMLElement>this.document.activeElement;
-      this.renderer.setAttribute(this.el.nativeElement, 'tabindex', '0');
-
-      this.addReboundEls();
     }
+
+    this.addReboundEls();
   }
 
   ngOnDestroy() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.removeReboundEls();
-    }
-
+    this.removeReboundEls();
     this.setPreviousFocus();
     this.focusTrapsTracker.activatePreviousTrapper();
   }

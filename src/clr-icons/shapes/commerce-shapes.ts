faf61649ac7687b38e8549d4733c9efdf45f018b
stsogoo <stsogoo@vmware.com>
2018-08-16 11:09:48 -0700

    [ICONS] Vol12 Icons
    - KS app: http://vol-12-icons.surge.sh/iconography
    
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/clr-icons/shapes/commerce-shapes.ts
+++ src/clr-icons/shapes/commerce-shapes.ts
@@ -161,6 +161,10 @@ export const ClrShapePeso = clrIconSVG(
   `<path d="M31,13.2H27.89A6.81,6.81,0,0,0,28,12a7.85,7.85,0,0,0-.1-1.19h2.93a.8.8,0,0,0,0-1.6H27.46A8.44,8.44,0,0,0,19.57,4H11a1,1,0,0,0-1,1V9.2H7a.8.8,0,0,0,0,1.6h3v2.4H7a.8.8,0,0,0,0,1.6h3V31a1,1,0,0,0,2,0V20h7.57a8.45,8.45,0,0,0,7.89-5.2H31a.8.8,0,0,0,0-1.6ZM12,6h7.57a6.51,6.51,0,0,1,5.68,3.2H12Zm0,4.8H25.87a5.6,5.6,0,0,1,0,2.4H12ZM19.57,18H12V14.8H25.25A6.51,6.51,0,0,1,19.57,18Z" class="clr-i-outline clr-i-outline-path-1" /><path d="M14.18,13.8V16h9.45a5.26,5.26,0,0,0,.08-.89,4.72,4.72,0,0,0-.2-1.31Z" class="clr-i-solid clr-i-solid-path-1" /><path d="M14.18,19.7h5.19a4.28,4.28,0,0,0,3.5-1.9H14.18Z" class="clr-i-solid clr-i-solid-path-2" /><path d="M19.37,10.51H14.18V12h8.37A4.21,4.21,0,0,0,19.37,10.51Z" class="clr-i-solid clr-i-solid-path-3" /><path d="M17.67,2a16,16,0,1,0,16,16A16,16,0,0,0,17.67,2Zm10.5,15.8H25.7a6.87,6.87,0,0,1-6.33,4.4H14.18v6.54a1.25,1.25,0,1,1-2.5,0V17.8H8.76a.9.9,0,1,1,0-1.8h2.92V13.8H8.76a.9.9,0,1,1,0-1.8h2.92V9.26A1.25,1.25,0,0,1,12.93,8h6.44a6.84,6.84,0,0,1,6.15,4h2.65a.9.9,0,0,1,0,1.8H26.09a6.91,6.91,0,0,1,.12,1.3,6.8,6.8,0,0,1-.06.9h2a.9.9,0,0,1,0,1.8Z" class="clr-i-solid clr-i-solid-path-4" />`
 );
 
+export const ClrShapeRuble = clrIconSVG(
+  `<path d="M20.57,20A8.23,8.23,0,0,0,29,12a8.23,8.23,0,0,0-8.43-8H12a1,1,0,0,0-1,1V18H9a1,1,0,0,0,0,2h2v2H9a1,1,0,0,0,0,2h2v7a1,1,0,0,0,2,0V24h9a1,1,0,0,0,0-2H13V20ZM13,6h7.57A6.24,6.24,0,0,1,27,12a6.23,6.23,0,0,1-6.43,6H13Z" class="clr-i-outline clr-i-outline-path-1" /><path d="M20.75,9.25H15v8.81h5.79a4.66,4.66,0,0,0,4.86-4.4A4.65,4.65,0,0,0,20.75,9.25Z" class="clr-i-solid clr-i-solid-path-1" /><path d="M18,2A16,16,0,1,0,34,18,16,16,0,0,0,18,2Zm2.75,18.56H15V22h8.29a1,1,0,0,1,0,2H15v5a1.25,1.25,0,0,1-2.5,0V24H11.25a1,1,0,0,1,0-2h1.21V20.56H11.25a1.25,1.25,0,0,1,0-2.5h1.21V8a1.25,1.25,0,0,1,1.25-1.25h7a7.14,7.14,0,0,1,7.36,6.9A7.15,7.15,0,0,1,20.75,20.56Z" class="clr-i-solid clr-i-solid-path-2" />`
+);
+
 export const CommerceShapes: any = {
   calculator: ClrShapeCalculator,
   'piggy-bank': ClrShapePiggyBank,
@@ -180,6 +184,7 @@ export const CommerceShapes: any = {
   won: ClrShapeWon,
   yen: ClrShapeYen,
   bitcoin: ClrShapeBitcoin,
+  ruble: ClrShapeRuble,
   'coin-bag': ClrShapeCoinBag,
 };
 

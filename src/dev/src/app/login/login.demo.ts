35473b855a43e9ab34c0986c0072043861c5cbbc
Jeff Kumar <jeffk@vmware.com>
2018-09-20 13:55:36 -0400

    [WEB] updating login page #2566
    Signed-off-by: Jeff Kumar <jeffk@vmware.com>

--- src/dev/src/app/login/login.demo.ts
+++ src/dev/src/app/login/login.demo.ts
@@ -3,7 +3,7 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 
 @Component({ selector: 'clr-login-demo', templateUrl: './login.demo.html' })
 export class LoginDemo {

08be94663cfe01e52d698cb2785dc6af80424a58
Scott Mathis <mathisscott@users.noreply.github.com>
2018-10-08 09:42:07 -0500

    [BUG] Making action red accessible against #FAFAFA background (#2706)
    • our previous red for outline buttons did not have sufficient contrast against the content background (#FAFAFA)
    • I adjusted the color down and it now has 4.59:1 contrast
    • after merge, I will backport to 0.13, 0.12, and 0.11
    
    Tested in:
    ✔︎ Chrome
    ✔︎ Firefox
    
    Closes: #2701
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/ks-app/src/app/containers/colors/colors.component.ts
+++ src/ks-app/src/app/containers/colors/colors.component.ts
@@ -108,7 +108,7 @@ export class KSColors {
       colors: [
         { value: '#a32100', text: 'light' },
         { value: '#c92100', text: 'light' },
-        { value: '#e62700', text: 'mid', corePalette: true },
+        { value: '#e12200', text: 'light', corePalette: true },
         { value: '#ebafa6', text: 'dark' },
         { value: '#f5dbd9', text: 'dark' },
       ],

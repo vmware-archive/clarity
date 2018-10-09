d0a1698ae40f8cf50d499b63eed5ae9adf03b52f
Jeeyun Lim <jeeyun.lim@gmail.com>
2018-10-09 16:29:27 -0700

    [UI] `clr-` prefix for css classes
    Signed-off-by: Jeeyun Lim <jeeyun.lim@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,25 @@
-module.exports = {
+module.exports = ctx => ({
   parser: '',
   plugins: [
     require('autoprefixer')({
       browsers: ['last 2 versions', 'ie 11', '> 5%', 'Firefox > 35', 'Chrome > 35'],
       cascade: false,
     }),
+    require('postcss-prefix-selector')({
+      prefix: '.clr-',
+      transform: function(prefix, selector, prefixedSelector) {
+        const regex = /\.((?!clr-)[a-zA-Z0-9_\-]*)/g;
+        const result = regex.exec(selector);
+        let updated = selector;
+        if (result) {
+          updated = updated.replace(regex, prefix + '$1');
+
+          if (ctx.env === 'deprecated') {
+            return selector + ', ' + updated;
+          }
+        }
+        return updated;
+      },
+    }),
   ],
-};
+});

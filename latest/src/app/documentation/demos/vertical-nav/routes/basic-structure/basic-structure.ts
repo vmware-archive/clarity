167c94ca32cae2a3ff49725582644f177453e705
Alex Mellnik <a.r.mellnik@gmail.com>
2018-10-13 21:10:03 -0700

    Docs: mention horizontal divider in vert nav
    This was suggested by a user in #1889.  Another user mentioned that the code and examples links were not working, but they seem to be fine now.  
    
    Signed-off-by: Alex Mellnik <a.r.mellnik@gmail.com>

--- latest/src/app/documentation/demos/vertical-nav/routes/basic-structure/basic-structure.ts
+++ latest/src/app/documentation/demos/vertical-nav/routes/basic-structure/basic-structure.ts
@@ -20,6 +20,7 @@ const HTML_EXAMPLE = `
             <a clrVerticalNavLink routerLink="./pikachu" routerLinkActive="active">Pikachu</a>
             <a clrVerticalNavLink routerLink="./raichu" routerLinkActive="active">Raichu</a>
             <a clrVerticalNavLink routerLink="./snorlax" routerLinkActive="active">Snorlax</a>
+            <div class="nav-divider"></div>
             <a clrVerticalNavLink routerLink="./credit" routerLinkActive="active">Credit</a>
         </clr-vertical-nav>
     </div>

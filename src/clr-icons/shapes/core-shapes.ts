e9907040502e1f0c92ccf6c8222f6d6ab6a65144
stsogoo <stsogoo@vmware.com>
2018-10-19 15:52:25 -0700

    [ICONS] Volume 14 icons
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/clr-icons/shapes/core-shapes.ts
+++ src/clr-icons/shapes/core-shapes.ts
@@ -302,6 +302,9 @@ export const ClrShapeEyeHide = clrIconSVG(
             <path d="M34.29,17.53c-3.37-6.23-9.28-10-15.82-10a16.82,16.82,0,0,0-5.24.85L14.84,10a14.78,14.78,0,0,1,3.63-.47c5.63,0,10.75,3.14,13.8,8.43a17.75,17.75,0,0,1-4.37,5.1l1.42,1.42a19.93,19.93,0,0,0,5-6l.26-.48Z" class="clr-i-solid clr-i-solid-path-2"/>
             <path d="M4.87,5.78l4.46,4.46a19.52,19.52,0,0,0-6.69,7.29L2.38,18l.26.48c3.37,6.23,9.28,10,15.82,10a16.93,16.93,0,0,0,7.37-1.69l5,5,1.75-1.5-26-26Zm8.3,8.3a6.85,6.85,0,0,0,9.55,9.55l1.6,1.6a14.91,14.91,0,0,1-5.86,1.2c-5.63,0-10.75-3.14-13.8-8.43a17.29,17.29,0,0,1,6.12-6.3Z" class="clr-i-solid clr-i-solid-path-3"/>`
 );
+export const ClrShapeStepForward2 = clrIconSVG(
+  `<path d="M7.08,6.52a1.68,1.68,0,0,0,0,2.4L16.51,18,7.12,27.08a1.7,1.7,0,0,0,2.36,2.44h0L21.4,18,9.48,6.47A1.69,1.69,0,0,0,7.08,6.52Z" class="clr-i-outline clr-i-outline-path-1" /><path d="M26.49,5a1.7,1.7,0,0,0-1.7,1.7V29.3a1.7,1.7,0,0,0,3.4,0V6.7A1.7,1.7,0,0,0,26.49,5Z" class="clr-i-outline clr-i-outline-path-2" />`
+);
 
 export const CoreShapes: any = {
   'unknown-status': ClrShapeUnknownStatus,
@@ -338,6 +341,7 @@ export const CoreShapes: any = {
   event: ClrShapeEvent,
   eye: ClrShapeEye,
   'eye-hide': ClrShapeEyeHide,
+  'step-forward-2': ClrShapeStepForward2,
 };
 
 Object.defineProperty(CoreShapes, 'house', descriptorConfig(CoreShapes.home));

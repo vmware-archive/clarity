6f1b5a3d03986e2db9678af5cdd680f604fc4ae5
Matt Hippely <mhippely@vmware.com>
2018-10-23 10:39:51 -0700

    [NG] fix(focust-trap): fixes #2766 for Clarity v0.11
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/clr-angular/utils/focus-trap/focus-trap.module.ts
+++ src/clr-angular/utils/focus-trap/focus-trap.module.ts
@@ -4,14 +4,21 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import {CommonModule} from "@angular/common";
-import {NgModule} from "@angular/core";
+import {NgModule, Optional, SkipSelf} from "@angular/core";
 import {FocusTrapTracker} from "./focus-trap-tracker.service";
 
 import {FOCUS_TRAP_DIRECTIVES} from "./index";
+import {ResponsiveNavigationService} from "../../layout/nav/providers/responsive-navigation.service";
+import {ResponsiveNavigationProvider} from "../../layout/nav/providers/responsive-navigation.provider";
+import {FocusTrapTrackerProvider} from "./focus-trap-tracker.provider";
 
 @NgModule({
     imports: [CommonModule],
-    providers: [FocusTrapTracker],
+    providers: [{
+        provide: FocusTrapTracker,
+        useFactory: FocusTrapTrackerProvider,
+        deps: [[new Optional(), new SkipSelf(), FocusTrapTracker]]
+    }],
     declarations: [FOCUS_TRAP_DIRECTIVES],
     exports: [FOCUS_TRAP_DIRECTIVES]
 })

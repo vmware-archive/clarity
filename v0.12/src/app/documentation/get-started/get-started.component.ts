70efa957da4643e00fd41c19f7a7f44f8c19b5bb
Sivakumar Annamalai <kpa.siva@hotmail.com>
2018-11-16 16:06:18 +0530

    [WEB] Fix typo in import statement (v0.12, v0.13, v1.0)
    Fix typo in BrowserAminationsModule import statement of the Get Started document
    
    Signed-off-by: Sivakumar Annamalai <kpa.siva@hotmail.com>

--- v0.12/src/app/documentation/get-started/get-started.component.ts
+++ v0.12/src/app/documentation/get-started/get-started.component.ts
@@ -3,7 +3,7 @@ import {Component} from "@angular/core";
 const NG_MODULE_EXAMPLE = `
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAminationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ClarityModule } from "@clr/angular";
 import { AppComponent } from "./app.component";
 

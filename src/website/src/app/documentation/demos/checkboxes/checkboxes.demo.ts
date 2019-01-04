22652649efc95e64b4c9be996af370c4a0757807
Jeremy Wilken <gnomation@gnomeontherun.com>
2019-01-04 16:23:19 -0600

    update radio and checkbox documentation for disabled and indeterminate states
    fixes #2943
    
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- src/website/src/app/documentation/demos/checkboxes/checkboxes.demo.ts
+++ src/website/src/app/documentation/demos/checkboxes/checkboxes.demo.ts
@@ -10,11 +10,14 @@ import * as UiBasic from './ui/basic.html';
 import * as UiFull from './ui/full.html';
 import * as UiError from './ui/error.html';
 import * as UiInline from './ui/inline.html';
+import * as UiDisabled from './ui/disabled.html';
 
 import * as NgBasic from './ng/basic.html';
 import * as NgLabel from './ng/label.html';
 import * as NgHelpers from './ng/helpers.html';
 import * as NgInline from './ng/inline.html';
+import * as NgDisabled from './ng/disabled.html';
+import * as NgIndeterminate from './ng/indeterminate.html';
 
 @Component({
   templateUrl: './checkboxes.demo.html',
@@ -31,14 +34,20 @@ export class CheckboxesDemo extends ClarityDocComponent {
   exampleOne = '';
   exampleTwo = '';
   exampleThree = '';
+  exampleFour = '';
 
   uiBasic: any = UiBasic;
   uiFull: any = UiFull;
   uiError: any = UiError;
   uiInline: any = UiInline;
+  uiDisabled: any = UiDisabled;
 
   ngBasic: any = NgBasic;
   ngLabel: any = NgLabel;
   ngHelpers: any = NgHelpers;
   ngInline: any = NgInline;
+  ngDisabled: any = NgDisabled;
+  ngIndeterminate: any = NgIndeterminate;
+
+  indeterminateState = true;
 }

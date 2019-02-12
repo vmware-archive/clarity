84112ec1dde9d455e69fc043f54dec591f2f4e42
Shijir Tsogoo <stsogoo@vmware.com>
2019-02-12 09:24:55 -0800

    [WIP] Vol14 icons (#3099)
    * [ICONS] vol14 icons
    
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/clr-icons/shapes/chart-shapes.ts
+++ src/clr-icons/shapes/chart-shapes.ts
@@ -218,6 +218,9 @@ export const ClrShapeTickChart = clrIconSVG(`
   <path class="clr-i-outline--alerted clr-i-outline-path-3--alerted clr-i-alert" d="M 26.854 1.144 L 21.134 11.004 C 20.579 11.818 21.114 12.928 22.097 13.001 C 22.142 13.005 22.188 13.006 22.234 13.004 L 33.684 13.004 C 34.669 13.036 35.319 11.991 34.855 11.122 C 34.834 11.081 34.81 11.042 34.784 11.004 L 29.064 1.144 C 28.57 0.299 27.348 0.299 26.854 1.144 Z"/>
   <path class="clr-i-solid--alerted clr-i-solid-path-1--alerted" d="M 34 29 C 34 30.105 33.105 31 32 31 L 4 31 C 2.896 31 2 30.105 2 29 L 2 7 C 2 5.896 2.896 5 4 5 L 21.958 5 L 17.625 12.395 C 16.795 13.601 17.594 15.245 19.064 15.351 C 19.134 15.357 19.201 15.359 19.27 15.357 L 34 15.357 Z M 6 26.991 L 27.723 26.99 C 28.483 26.93 28.903 26.06 28.463 25.43 C 28.293 25.18 28.023 25.02 27.723 25 L 25 25 L 25 22 L 23 22 L 23 25 L 20 25 L 20 22 L 18 22 L 18 25 L 15 25 L 15 22 L 13 22 L 13 25 L 10 25 L 10 22 L 8 22 L 8 25 L 6 25 Z"/>
   <path class="clr-i-solid--alerted clr-i-solid-path-2--alerted clr-i-alert" d="M 26.854 1.144 L 21.134 11.004 C 20.579 11.818 21.114 12.928 22.097 13.001 C 22.142 13.005 22.188 13.006 22.234 13.004 L 33.684 13.004 C 34.669 13.036 35.319 11.991 34.855 11.122 C 34.834 11.081 34.81 11.042 34.784 11.004 L 29.064 1.144 C 28.57 0.299 27.348 0.299 26.854 1.144 Z"/>`);
+export const ClrShapeBellCurve = clrIconSVG(
+  `<path d="M33,29H3A1,1,0,1,1,3,27H33A1,1,0,1,1,33,29Z" class="clr-i-outline clr-i-outline-path-1" /><path d="M33,25h-.62a8.11,8.11,0,0,1-8-6.67C23.62,14.44,21.89,7.94,18,7.94s-5.69,6.51-6.38,10.39a8.11,8.11,0,0,1-8,6.65H3a1,1,0,1,1,0-2h.6A6.11,6.11,0,0,0,9.6,18c1.41-7.88,4.3-12,8.35-12s6.93,4.16,8.33,12a6.11,6.11,0,0,0,6,5H33a1,1,0,0,1,0,2Z" class="clr-i-outline clr-i-outline-path-2" />`
+);
 
 export const ChartShapes: any = {
   'axis-chart': ClrShapeAxisChart,
@@ -232,6 +235,7 @@ export const ChartShapes: any = {
   'scatter-plot': ClrShapeScatterPlot,
   'box-plot': ClrShapeBoxPlot,
   'heat-map': ClrShapeHeatMap,
+  'bell-curve': ClrShapeBellCurve,
 };
 
 Object.defineProperty(ChartShapes, 'analytics', descriptorConfig(ChartShapes['line-chart']));

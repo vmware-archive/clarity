012f1bd821dccd48c66c7b377ff90ce8e8e9a9b1
Megha Sachdev <meghasachdev12@gmail.com>
2019-03-13 12:40:28 -0700

    lint fixes
    Signed-off-by: Megha Sachdev <meghasachdev12@gmail.com>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,7 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import '@applitools/eyes-cypress/commands';
 
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
--- cypress/util.js
+++ cypress/util.js
@@ -1,10 +1,9 @@
 export const baseURL = 'http://localhost:4200/';
 
-
 export function checkEyes(testName) {
-    cy.eyesCheckWindow({
-       tag: testName,
-       sizeMode: 'selector', //mode
-       selector: '.content-area'
-      });
-  }
+  cy.eyesCheckWindow({
+    tag: testName,
+    sizeMode: 'selector', //mode
+    selector: '.content-area',
+  });
+}

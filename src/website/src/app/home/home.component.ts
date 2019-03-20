938c0401868039ad57b52055aa2baca0aa941519
Cory Rylan <2021067+coryrylan@users.noreply.github.com>
2019-03-20 12:09:50 -0500

    [WEBSITE] spring logo update (#3225)
    * [WEBSITE] spring logo update
    
    Adds new spring logo as well as optimizing the website svg files.
    
    closes #3223
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/website/src/app/home/home.component.ts
+++ src/website/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -12,4 +12,29 @@ import { Component } from '@angular/core';
     '[class.content-container]': 'true',
   },
 })
-export class HomeComponent {}
+export class HomeComponent {
+  currentSeason = `bg-img-${this.getCurrentSeason()}`;
+
+  private getCurrentSeason() {
+    const now = new Date();
+    const currentYear = now.getFullYear();
+
+    if (now < new Date(currentYear, 2, 1)) {
+      return 'winter'; // Jan 1 - Mar 1
+    }
+
+    if (now < new Date(currentYear, 5, 1)) {
+      return 'spring'; // Mar 1 - Jun 1
+    }
+
+    if (now < new Date(currentYear, 8, 1)) {
+      return 'summer'; // Jun 1 - Sep 1
+    }
+
+    if (now < new Date(currentYear, 11, 1)) {
+      return 'autumn'; // Sep 1 - Dec 1
+    }
+
+    return 'winter'; // Dec 1 - Dec 31
+  }
+}

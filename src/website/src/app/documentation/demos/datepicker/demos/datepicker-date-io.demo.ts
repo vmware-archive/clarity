152eb3a2cdaddb46729ffeb65c39fbb4e425733c
Matt Hippely <mhippely@vmware.com>
2019-04-08 10:28:49 -0700

    chore(datepicker): Removes newFormLayout examples and api reference from the website and docs.
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/website/src/app/documentation/demos/datepicker/demos/datepicker-date-io.demo.ts
+++ src/website/src/app/documentation/demos/datepicker/demos/datepicker-date-io.demo.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 
 const HTML_EXAMPLE = `
 <form clrForm clrLayout="vertical">
-  <input type="date" [(clrDate)]="date" newFormLayout="true">
+  <input type="date" [(clrDate)]="date">
 </form>
 `;
 

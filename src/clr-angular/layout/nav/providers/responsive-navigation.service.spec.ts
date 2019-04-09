8e8af1d5b1796eed783409c9221a817ec0bc4dd2
Cory Rylan <2021067+coryrylan@users.noreply.github.com>
2019-04-09 12:01:34 -0500

    [NG] fix header nav menu button (#3263)
    Menu button missing if header was added dynamically.
    
    closes #3254
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/clr-angular/layout/nav/providers/responsive-navigation.service.spec.ts
+++ src/clr-angular/layout/nav/providers/responsive-navigation.service.spec.ts
@@ -6,6 +6,7 @@
 // Isolated tests for services: https://angular.io/docs/ts/latest/guide/testing.html#!#isolated-service-tests
 import { ResponsiveNavCodes } from '../responsive-nav-codes';
 import { ResponsiveNavigationService } from './responsive-navigation.service';
+import { ResponsiveNavControlMessage } from '../responsive-nav-control-message';
 
 describe('ResponsiveNavigationService', () => {
   let service: ResponsiveNavigationService;
@@ -43,5 +44,29 @@ describe('ResponsiveNavigationService', () => {
     expect(service.responsiveNavList.length).toBe(1);
     service.unregisterNav(ResponsiveNavCodes.NAV_LEVEL_1);
     expect(service.responsiveNavList.length).toBe(0);
+  });
+
+  it('should notify updates of registered navs', () => {
+    let navs = [];
+    service.registeredNavs.subscribe(n => (navs = n));
+    expect(navs.length).toBe(0);
+    service.registerNav(ResponsiveNavCodes.NAV_LEVEL_1);
+    expect(navs.length).toBe(1);
+  });
+
+  it('should notify if subscription is late, example: header is dynamically added later', () => {
+    let navs = [];
+    service.registerNav(ResponsiveNavCodes.NAV_LEVEL_1);
+    service.registeredNavs.subscribe(n => (navs = n));
+    expect(navs.length).toBe(1);
+  });
+
+  it('should send control messages to nav controls', () => {
+    let controlMessage: ResponsiveNavControlMessage = null;
+    service.navControl.subscribe(message => (controlMessage = message));
+    expect(controlMessage).toBe(null);
+    service.sendControlMessage('control_code', 1);
+    expect(controlMessage.controlCode).toBe('control_code');
+    expect(controlMessage.navLevel).toBe(1);
   });
 });

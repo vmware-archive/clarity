4f4042a3291a5a67c76b7a04a0ae749785b6da93
Cory Rylan <2021067+coryrylan@users.noreply.github.com>
2019-04-19 09:13:03 -0500

    [NG] a11y improvements for control errors (#3305)
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/clr-angular/forms/common/error.ts
+++ src/clr-angular/forms/common/error.ts
@@ -1,16 +1,39 @@
 /**
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { Component } from '@angular/core';
+import { Component, Optional, HostBinding, Input } from '@angular/core';
+import { ControlIdService } from './providers/control-id.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'clr-control-error',
   template: `
     <ng-content></ng-content>
     `,
-  host: { '[class.clr-subtext]': 'true' },
+  host: {
+    '[class.clr-subtext]': 'true',
+    '[attr.aria-live]': '"polite"',
+  },
 })
-export class ClrControlError {}
+export class ClrControlError {
+  @Input('aria-describedby')
+  @HostBinding('attr.aria-describedby')
+  describedByAttr: string = null;
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(@Optional() private controlIdService: ControlIdService) {}
+
+  ngOnInit() {
+    if (this.controlIdService && !this.describedByAttr) {
+      this.subscriptions.push(this.controlIdService.idChange.subscribe(id => (this.describedByAttr = id)));
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+}

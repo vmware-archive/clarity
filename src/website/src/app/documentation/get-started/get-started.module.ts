9150d16b6012be1c86e3c5819250b818ae8dc9ba
Cory Rylan <crylan@vmware.com>
2019-04-23 22:20:20 -0500

    [WEBSITE] isolate component specific styles
    Convert components styles to use encapsulation. Delete dead code files.
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/website/src/app/documentation/get-started/get-started.module.ts
+++ src/website/src/app/documentation/get-started/get-started.module.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -16,7 +16,6 @@ const route: Routes = [
     path: '',
     component: GetStartedComponent,
     data: {
-      // bodyClass: "layout-get-started",
       browserTitle: 'Get Started',
     },
   },
--- src/website/src/app/news/news.component.ts
+++ src/website/src/app/news/news.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -17,6 +17,7 @@ import * as RELEASES from '../../releases/release-list.json';
 @Component({
   selector: 'news',
   templateUrl: '../../releases/final-template/auto-generated-news.component.html',
+  styleUrls: ['./news.component.scss'],
   host: {
     '[class.content-container]': 'true',
   },

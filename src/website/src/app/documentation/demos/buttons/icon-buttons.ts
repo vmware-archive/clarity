b8de4216a32d04efc4e966256dce941d739d982f
Cory Rylan <crylan@vmware.com>
2019-06-13 22:22:35 -0500

    [WEBSITE] update icon button examples a11y
    closes #3188
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/website/src/app/documentation/demos/buttons/icon-buttons.ts
+++ src/website/src/app/documentation/demos/buttons/icon-buttons.ts
@@ -1,27 +1,27 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from '@angular/core';
 
 const HTML_EXAMPLE = `
-<button type="button" class="btn btn-icon">
+<button type="button" class="btn btn-icon" aria-label="home">
     <clr-icon shape="home"></clr-icon>
 </button>
-<button type="button" class="btn btn-icon btn-primary">
+<button type="button" class="btn btn-icon btn-primary" aria-label="settings">
     <clr-icon shape="cog"></clr-icon>
 </button>
-<button type="button" class="btn btn-icon btn-warning">
+<button type="button" class="btn btn-icon btn-warning" aria-label="warning">
     <clr-icon shape="warning-standard"></clr-icon>
 </button>
-<button type="button" class="btn btn-icon btn-danger">
+<button type="button" class="btn btn-icon btn-danger" aria-label="error">
     <clr-icon shape="error-standard"></clr-icon>
 </button>
-<button type="button" class="btn btn-icon btn-success">
+<button type="button" class="btn btn-icon btn-success" aria-label="done">
     <clr-icon shape="check"></clr-icon>
 </button>
-<button type="button" class="btn btn-icon" disabled>
+<button type="button" class="btn btn-icon" disabled aria-label="home">
     <clr-icon shape="home"></clr-icon>
 </button>
 `;

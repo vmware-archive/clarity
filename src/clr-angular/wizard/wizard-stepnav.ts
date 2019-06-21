6a12fb8dd529439def44969e3b15171d0a4898ab
Cory Rylan <crylan@vmware.com>
2019-06-21 10:10:59 -0500

    [NG] wizard remove tab roles
    issue #3399
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/clr-angular/wizard/wizard-stepnav.ts
+++ src/clr-angular/wizard/wizard-stepnav.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -10,11 +10,10 @@ import { PageCollectionService } from './providers/page-collection.service';
 @Component({
   selector: 'clr-wizard-stepnav',
   template: `
-        <ol class="clr-wizard-stepnav-list" role="tablist">
-            <li *ngFor="let page of pageService.pages" clr-wizard-stepnav-item 
-            [page]="page" class="clr-wizard-stepnav-item"></li>
-        </ol>
-    `,
+    <div class="clr-wizard-stepnav-list">
+      <div *ngFor="let page of pageService.pages" clr-wizard-stepnav-item [page]="page" class="clr-wizard-stepnav-item"></div>
+    </div>
+  `,
   host: { class: 'clr-wizard-stepnav' },
 })
 export class ClrWizardStepnav {

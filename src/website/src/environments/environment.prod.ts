77e292002cef46987c8145e1eeb2f7f133d83f93
stsogoo <stsogoo@vmware.com>
2019-06-25 12:44:54 -0700

    [WEBSITE] fixes resource links
    - updated url to sketch 2.0.0 (closes #3491)
    - fixed figma urls in 2.0.0 release ntoes (closes #3451)
    
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- src/website/src/environments/environment.prod.ts
+++ src/website/src/environments/environment.prod.ts
@@ -12,7 +12,7 @@ export const environment = {
   icons_url: 'https://github.com/vmware/clarity-assets/archive/master.zip',
   sketch_base_url: 'https://github.com/vmware/clarity-assets/raw/master/sketch',
   sketch_icons_url:
-    'https://github.com/vmware/clarity-assets/raw/master/sketch/icons/clarity-library-icons-1.0.0.sketch',
+    'https://github.com/vmware/clarity-assets/raw/master/sketch/icons/clarity-library-icons-2.0.0.sketch',
   figma_link_light: 'https://www.figma.com/file/nRpy4E8bYy9hiDc8iORxyp/Clarity-UI-Library-light-2.0.0?node-id=262%3A7',
   figma_link_dark: 'https://www.figma.com/file/b0FLd4CGw6JIXZ8kwXKaE6/Clarity-UI-Library-dark-2.0.0?node-id=88%3A0',
   figma_icons: 'https://www.figma.com/file/Ju57cw9zRBQ77OMrbKD5Ko/Clarity-Icons-2.0.0?node-id=54%3A9',

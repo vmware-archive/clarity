07a2d0a5f98c616e44798b1098deaaa1fe038286
smathis <smathis@vmware.com>
2019-06-27 15:40:50 -0500

    [ICONS] Adding vol.15 icons
    • Includes more text-edit icons (Text Editor)
    • Includes all kubernetes icons (Technology)
    • Includes a "beta" icon (Essentials)
    • Includes a storage adapter icon (Technology)
    • Verified locally by building website and also verified full build
    
    Tested in:
    ✔︎ Chrome
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/clr-icons/shapes/essential-shapes.ts
+++ src/clr-icons/shapes/essential-shapes.ts
@@ -900,6 +900,14 @@ export const ClrShapeCircle = clrIconSVG(
 export const ClrShapeDotCircle = clrIconSVG(
   `<path d="M18,11a7,7,0,1,1-7,7,7,7,0,0,1,7-7" class="clr-i-outline clr-i-outline-path-1" /><path d="M18,34A16,16,0,1,1,34,18,16,16,0,0,1,18,34ZM18,4A14,14,0,1,0,32,18,14,14,0,0,0,18,4Z" class="clr-i-outline clr-i-outline-path-2" />`
 );
+// TODO: Beta needs solid version
+export const ClrShapeBeta = clrIconSVG(
+  `<path d="M7.21,14.07h3a1.61,1.61,0,0,1,1.81,1.5,1.44,1.44,0,0,1-.84,1.34,1.67,1.67,0,0,1,1.1,1.53,1.75,1.75,0,0,1-2,1.63H7.21Zm2.71,2.42c.48,0,.82-.28.82-.67s-.34-.65-.82-.65H8.49v1.32Zm.2,2.48a.75.75,0,1,0,0-1.47H8.49V19Z" class="clr-i-outline clr-i-outline-path-1"/>
+  <path d="M14.55,15.23v1.2h3v1.16h-3v1.32h3.33v1.16H13.26v-6h4.62v1.16Z" class="clr-i-outline clr-i-outline-path-2"/>
+  <path d="M20.41,15.23H18.54V14.07h5v1.16H21.7v4.84H20.41Z" class="clr-i-outline clr-i-outline-path-3"/>
+  <path d="M28,19.12H25.32l-.38.95H23.5l2.44-6h1.44l2.45,6H28.38ZM27.55,18l-.89-2.19L25.77,18Z" class="clr-i-outline clr-i-outline-path-4"/>
+  <path d="M8.06,30a.84.84,0,0,1-.38-.08A1,1,0,0,1,7.06,29V25h-4a1,1,0,0,1-1-1V10a1,1,0,0,1,1-1h30a1,1,0,0,1,1,1V24a1,1,0,0,1-1,1H13.48L8.77,29.71A1,1,0,0,1,8.06,30Zm-4-7h4a1,1,0,0,1,1,1v2.59l3.3-3.3a1,1,0,0,1,.7-.29h19V11h-28Z" class="clr-i-outline clr-i-outline-path-5"/>`
+);
 
 export const EssentialShapes: any = {
   'add-text': ClrShapeAddText,
@@ -1025,6 +1033,7 @@ export const EssentialShapes: any = {
   'dot-circle': ClrShapeDotCircle,
   volume: ClrShapeVolume,
   crosshairs: ClrShapeCrosshairs,
+  beta: ClrShapeBeta,
 };
 
 Object.defineProperty(EssentialShapes, 'edit', descriptorConfig(EssentialShapes.pencil));

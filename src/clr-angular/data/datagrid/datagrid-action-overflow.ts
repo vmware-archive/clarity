83239c30cb5b252c29835b18806f1a31d21f86c0
Ivan Donchev <idonchev@vmware.com>
2019-07-01 11:59:49 +0300

    Backport to V1: Datagrid focus management (#3370) (#3481) (#3535)
    * Backport to V1: Datagrid focus management (#3370) (#3481)
    
    * Page count select focus management
    * Pagination focus management
    * Filter focus and action overflow 1st item focus
    
    (cherry picked from commit 0dc08f9fced04c705f6bf8fd99f49c7a9c7daad7)
    
    Signed-off-by: Ivan Donchev <idonchev@vmware.com>

--- src/clr-angular/data/datagrid/datagrid-action-overflow.ts
+++ src/clr-angular/data/datagrid/datagrid-action-overflow.ts
@@ -1,36 +1,61 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  ElementRef,
+  NgZone,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
 
 import { Point } from '../../popover/common/popover';
 
 import { RowActionService } from './providers/row-action-service';
 import { ClrCommonStrings } from '../../utils/i18n/common-strings.interface';
+import { isPlatformBrowser } from '@angular/common';
+
+let clrDgActionId = 0;
 
 @Component({
   selector: 'clr-dg-action-overflow',
   template: `
-        <button (click)="toggle($event)" type="button" class="datagrid-action-toggle" #anchor>
+        <button (click)="toggle($event)" type="button" class="datagrid-action-toggle" #anchor role="button" 
+                    aria-haspopup="true" [attr.aria-controls]="popoverId" [attr.aria-expanded]="open">
             <clr-icon shape="ellipsis-vertical" [attr.title]="commonStrings.rowActions"></clr-icon>
         </button>
         <ng-template [(clrPopoverOld)]="open" [clrPopoverOldAnchor]="anchor" [clrPopoverOldAnchorPoint]="anchorPoint"
                      [clrPopoverOldPopoverPoint]="popoverPoint">
-            <div #menu class="datagrid-action-overflow" (clrOutsideClick)="close($event)" [clrStrict]="true">
+            <div [attr.class]="overflowClassName" (clrOutsideClick)="close($event)" [clrStrict]="true" 
+                    role="menu" [attr.id]="popoverId" [attr.aria-hidden]="!open">
                 <ng-content></ng-content>
             </div>
         </ng-template>
     `,
 })
 export class ClrDatagridActionOverflow implements OnDestroy {
+  public overflowClassName = 'datagrid-action-overflow';
   public anchorPoint: Point = Point.RIGHT_CENTER;
   public popoverPoint: Point = Point.LEFT_CENTER;
 
-  constructor(private rowActionService: RowActionService, public commonStrings: ClrCommonStrings) {
+  constructor(
+    private rowActionService: RowActionService,
+    public commonStrings: ClrCommonStrings,
+    private elementRef: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private zone: NgZone
+  ) {
     this.rowActionService.register();
+    this.popoverId = 'clr-action-menu' + clrDgActionId++;
   }
+
+  popoverId: string;
 
   ngOnDestroy() {
     this.rowActionService.unregister();
@@ -50,6 +75,18 @@ export class ClrDatagridActionOverflow implements OnDestroy {
     if (boolOpen !== this._open) {
       this._open = boolOpen;
       this.openChanged.emit(boolOpen);
+      if (boolOpen && isPlatformBrowser(this.platformId)) {
+        this.zone.runOutsideAngular(() => {
+          setTimeout(() => {
+            const firstButton = this.elementRef.nativeElement.querySelector(
+              `.${this.overflowClassName} button.action-item`
+            );
+            if (firstButton) {
+              firstButton.focus();
+            }
+          });
+        });
+      }
     }
   }
 

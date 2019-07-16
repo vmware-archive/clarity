a04c378a82eaae38be6f901a338f680443592625
smathis <smathis@vmware.com>
2019-07-16 11:58:46 -0500

    fix: adding the text-edit set to the svg icon build script
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- scripts/clr-icons-svg.js
+++ scripts/clr-icons-svg.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -16,6 +16,7 @@ const SHAPE_SETS = [
   'technology-shapes',
   'travel-shapes',
   'chart-shapes',
+  'text-edit-shapes',
 ];
 
 writeSVGIcons(SHAPE_SETS, () => {

2da6c27c2448c373d0a956591dfcbbd01fbd6372
Cory Rylan <crylan@vmware.com>
2019-08-04 12:06:14 -0500

    build: update style lint
    - Update style lint to support sass syntax.
    - Enable build errors.
    - Disable noise errors temporary.
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/website/src/app/page-not-found/page-not-found.component.ts
+++ src/website/src/app/page-not-found/page-not-found.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'page-not-found',
   templateUrl: 'page-not-found.component.html',
-  styleUrls: ['page-not-found.component.css'],
+  styleUrls: ['page-not-found.component.scss'],
   host: {
     '[class.content-container]': 'true',
   },

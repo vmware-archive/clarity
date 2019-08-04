ab8124cbf9f5eced005df2a5e66e487308a6ce69
Cory Rylan <crylan@vmware.com>
2019-08-04 12:06:14 -0500

    build: update style lint
    - Update style lint to support sass syntax.
    - Enable build errors.
    - Disable noise errors temporary.
    
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/website/src/app/documentation/utils/new-layout-alert.component.ts
+++ src/website/src/app/documentation/utils/new-layout-alert.component.ts
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'clrweb-new-layout-alert',
+  selector: 'clr-web-new-layout-alert',
   template: `
     <div class="alert alert-info">
         <div class="alert-item">

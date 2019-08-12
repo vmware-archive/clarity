61009e6b4fcb38b12cd225df5c98970335f51613
Matt Hippely <mhippely@vmware.com>
2019-08-12 08:49:32 -0700

    fix: pr change (jeeyun and shijir)
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/clr-angular/popover/tooltip/providers/tooltip-id.service.ts
+++ src/clr-angular/popover/tooltip/providers/tooltip-id.service.ts
@@ -5,8 +5,7 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class TooltipIdService {

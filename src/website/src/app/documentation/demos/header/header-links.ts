df971facbd57b9660df91974ca161b308d2a01f8
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2019-08-14 13:17:24 +0300

    docs(header): header have buttons without accessible names #3518
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/website/src/app/documentation/demos/header/header-links.ts
+++ src/website/src/app/documentation/demos/header/header-links.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -15,7 +15,7 @@ const EXAMPLE = `
         <a href="javascript://" class="nav-link nav-text">Interactive Analytics</a>
     </div>
     <div class="header-actions">
-        <a href="javascript://" class="nav-link nav-icon">
+        <a href="javascript://" class="nav-link nav-icon" aria-label="settings">
             <clr-icon shape="cog"></clr-icon>
         </a>
     </div>

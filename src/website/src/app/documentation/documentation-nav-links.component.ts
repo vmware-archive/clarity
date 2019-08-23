f8d4a0cdd66272da11103e3723a014c90583be8a
Florin Borceanu <florin.borceanu@bytex.ro>
2019-08-23 11:48:04 +0300

    bug(sidenav): Sidenav in website documentation page doesn't collapse
    #3704
    
    Signed-off-by: Florin Borceanu <florin.borceanu@bytex.ro>

--- src/website/src/app/documentation/documentation-nav-links.component.ts
+++ src/website/src/app/documentation/documentation-nav-links.component.ts
@@ -8,23 +8,21 @@ import { Component, Input } from '@angular/core';
 import * as COMPONENTS from '../../settings/componentlist.json';
 
 @Component({
-  selector: 'documentation-nav-links',
-  template: `
-    <ul class="nav-list">
-        <ng-container *ngFor="let component of components">
-            <li *ngIf="component.url && !component.noDemo && component.type == type">
-                <a class="nav-link" [routerLink]="component.url" routerLinkActive="active">
-                    {{component.text}}
-                    <span *ngIf="component.isNew" class="new nav-link-tag">New!</span>
-                    <span *ngIf="component.isUpdated" class="updated nav-link-tag">Updated</span>
-                </a>
-            </li>
-        </ng-container>
-    </ul>
+    selector: 'documentation-nav-links',
+    template: `
+    <ng-container *ngFor="let component of components">
+        <li *ngIf="component.url && !component.noDemo && component.type == type">
+            <a class="nav-link" [routerLink]="component.url" routerLinkActive="active">
+                {{component.text}}
+                <span *ngIf="component.isNew" class="new nav-link-tag">New!</span>
+                <span *ngIf="component.isUpdated" class="updated nav-link-tag">Updated</span>
+            </a>
+        </li>
+    </ng-container>
     `,
 })
 export class DocumentationNavLinksComponent {
-  components = COMPONENTS.list;
+    components = COMPONENTS.list;
 
-  @Input() type: string;
+    @Input() type: string;
 }

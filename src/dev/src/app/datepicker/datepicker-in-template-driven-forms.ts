0bb9f0bb068475c7dbebae26f825b8925defc13a
Yuchen Xie <xp@yintrust.com>
2019-09-08 21:58:20 +0800

    Remove outdated comment
    Signed-off-by: Yuchen Xie <xp@yintrust.com>

--- src/dev/src/app/datepicker/datepicker-in-template-driven-forms.ts
+++ src/dev/src/app/datepicker/datepicker-in-template-driven-forms.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'clr-datepicker-in-template-driven-forms-demo',
-  // Note the .css extension here, not .scss. That's the best we can have at the moment.
   styleUrls: ['./datepicker.demo.scss'],
   templateUrl: './datepicker-in-template-driven-forms.html',
 })

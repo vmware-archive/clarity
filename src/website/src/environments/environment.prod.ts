3865cbe913fd2d566f899c2b3bac8aa72667ab08
Cory Rylan <crylan@vmware.com>
2019-09-27 09:34:21 -0500

    chore: update design resources for v2 docs
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/website/src/environments/environment.prod.ts
+++ src/website/src/environments/environment.prod.ts
@@ -12,8 +12,9 @@ export const environment = {
   icons_url: 'https://github.com/vmware/clarity-assets/archive/master.zip',
   sketch_base_url: 'https://github.com/vmware/clarity-assets/raw/master/sketch',
   sketch_icons_url:
-    'https://github.com/vmware/clarity-assets/raw/master/sketch/icons/clarity-library-icons-2.0.0.sketch',
-  figma_link_light: 'https://www.figma.com/file/nRpy4E8bYy9hiDc8iORxyp/Clarity-UI-Library-light-2.0.0?node-id=262%3A7',
-  figma_link_dark: 'https://www.figma.com/file/b0FLd4CGw6JIXZ8kwXKaE6/Clarity-UI-Library-dark-2.0.0?node-id=88%3A0',
-  figma_icons: 'https://www.figma.com/file/Ju57cw9zRBQ77OMrbKD5Ko/Clarity-Icons-2.0.0?node-id=54%3A9',
+    'https://github.com/vmware/clarity-assets/raw/master/sketch/icons/clarity-library-icons-2.2.0.sketch',
+  figma_link_light: 'https://www.figma.com/file/v2mkhzKQdhECXOx8BElgdA/Clarity-UI-Library-light-2.2.0',
+  figma_link_dark: 'https://www.figma.com/file/wRYSrWSffZXcdQuiolwkym/Clarity-UI-Library-dark-2.2.0',
+  figma_icons: 'https://www.figma.com/file/40gaW7S2yP7B0ME6QNDEsR/Clarity-Icons-2.2.0',
+  figma_color: 'https://www.figma.com/file/mxn3xoLg2mkjMqf5GGYUzw/Clarity-Colors-2.2.0',
 };

df5928e50bfc99afbce1b72b9762b514b731526c
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2019-11-08 16:11:52 +0200

    fix: datagrid clrDgFilterOpenChange was not fired
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/dev/src/app/datagrid/filtering/filtering.ts
+++ src/dev/src/app/datagrid/filtering/filtering.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -21,5 +21,9 @@ export class DatagridFilteringDemo {
     inventory.size = 10;
     inventory.reset();
     this.users = inventory.all;
+  }
+
+  dgFilterChange($event: boolean) {
+    console.log('clrDgFilterOpenChange is fired', $event);
   }
 }

a5eca507544d37a17f3f772bbc56d451075dfd71
smathis <smathis@vmware.com>
2019-12-30 14:37:48 -0600

    feat(icons): migrating chart icon set to clr-core
    Signed-off-by: Scott Mathis <smathis@vmware.com>

---
+++ src/clr-core/icon-shapes/collections/chart.ts
@@ -0,0 +1,73 @@
+/*
+* Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+* This software is released under MIT license.
+* The full license information can be found in LICENSE in the root directory of this project.
+*/
+
+import { ClarityIcons } from '../icon.service';
+import { IconAlias, IconShapeTuple } from '../interfaces/icon.interfaces';
+
+import { axisChartIcon, axisChartIconName } from '../shapes/axis-chart';
+import { barChartIcon, barChartIconName } from '../shapes/bar-chart';
+import { bellCurveIcon, bellCurveIconName } from '../shapes/bell-curve';
+import { boxPlotIcon, boxPlotIconName } from '../shapes/box-plot';
+import { bubbleChartIcon, bubbleChartIconName } from '../shapes/bubble-chart';
+import { cloudChartIcon, cloudChartIconName } from '../shapes/cloud-chart';
+import { curveChartIcon, curveChartIconName } from '../shapes/curve-chart';
+import { gridChartIcon, gridChartIconName } from '../shapes/grid-chart';
+import { heatMapIcon, heatMapIconName } from '../shapes/heat-map';
+import { lineChartIcon, lineChartIconName } from '../shapes/line-chart';
+import { pieChartIcon, pieChartIconName } from '../shapes/pie-chart';
+import { scatterPlotIcon, scatterPlotIconName } from '../shapes/scatter-plot';
+import { tickChartIcon, tickChartIconName } from '../shapes/tick-chart';
+
+export const chartCollectionIcons: IconShapeTuple[] = [
+  axisChartIcon,
+  barChartIcon,
+  bellCurveIcon,
+  boxPlotIcon,
+  bubbleChartIcon,
+  cloudChartIcon,
+  curveChartIcon,
+  gridChartIcon,
+  heatMapIcon,
+  lineChartIcon,
+  pieChartIcon,
+  scatterPlotIcon,
+  tickChartIcon,
+];
+
+export const chartCollectionAliases: IconAlias[] = [[lineChartIconName, ['analytics']]];
+
+/**
+ * Function that can be called to load the core icon set.
+ *
+ * ```typescript
+ * import { loadChartIconSet } from '@clr/core/icon';
+ *
+ * loadChartIconSet();
+ * ```
+ *
+ */
+export function loadChartIconSet() {
+  ClarityIcons.addIcons(...chartCollectionIcons);
+  ClarityIcons.addAliases(...chartCollectionAliases);
+}
+
+declare module '@clr/core/common' {
+  interface IconRegistrySources {
+    [axisChartIconName]?: string;
+    [barChartIconName]?: string;
+    [bellCurveIconName]?: string;
+    [boxPlotIconName]?: string;
+    [bubbleChartIconName]?: string;
+    [cloudChartIconName]?: string;
+    [curveChartIconName]?: string;
+    [gridChartIconName]?: string;
+    [heatMapIconName]?: string;
+    [lineChartIconName]?: string;
+    [pieChartIconName]?: string;
+    [scatterPlotIconName]?: string;
+    [tickChartIconName]?: string;
+  }
+}
---
+++ src/clr-core/icon-shapes/shapes/bar-chart.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { renderIcon } from '../icon.renderer';
+import { IconShapeTuple } from '../interfaces/icon.interfaces';
+
+const icon = {
+  outline:
+    '<path d="M32,5H4A2,2,0,0,0,2,7V29a2,2,0,0,0,2,2H32a2,2,0,0,0,2-2V7A2,2,0,0,0,32,5ZM4,29V7H32V29Z"/><path d="M 7 10 L 13 10 L 13 26 L 11.4 26 L 11.4 11.6 L 8.6 11.6 L 8.6 26 L 7 26 Z"/><path d="M 15 19 L 21 19 L 21 26 L 19.4 26 L 19.4 20.6 L 16.6 20.6 L 16.6 26 L 15 26 Z"/><path d="M 23 16 L 29 16 L 29 26 L 27.4 26 L 27.4 17.6 L 24.6 17.6 L 24.6 26 L 23 26 Z"/>',
+
+  outlineAlerted:
+    '<path d="M 34 29 C 34 30.105 33.105 31 32 31 L 4 31 C 2.895 31 2 30.105 2 29 L 2 7 C 2 5.895 2.895 5 4 5 L 21.958 5 L 20.786 7 L 4 7 L 4 29 L 32 29 L 32 15.357 L 34 15.357 Z"/><path d="M 7 10 L 13 10 L 13 26 L 11.4 26 L 11.4 11.6 L 8.6 11.6 L 8.6 26 L 7 26 Z"/><path d="M 15 19 L 21 19 L 21 26 L 19.4 26 L 19.4 20.6 L 16.6 20.6 L 16.6 26 L 15 26 Z"/><path d="M 23 16 L 29 16 L 29 26 L 27.4 26 L 27.4 17.6 L 24.6 17.6 L 24.6 26 L 23 26 Z"/>',
+
+  outlineBadged:
+    '<path d="M 32 13.22 L 32 29 L 4 29 L 4 7 L 22.57 7 C 22.524 6.668 22.501 6.334 22.5 6 C 22.501 5.665 22.524 5.331 22.57 5 L 4 5 C 2.895 5 2 5.895 2 7 L 2 29 C 2 30.104 2.895 31 4 31 L 32 31 C 33.105 31 34 30.104 34 29 L 34 12.34 C 33.38 12.73 32.706 13.026 32 13.22 Z"/><path d="M 7 10 L 13 10 L 13 26 L 11.4 26 L 11.4 11.6 L 8.6 11.6 L 8.6 26 L 7 26 Z"/><path d="M 15 19 L 21 19 L 21 26 L 19.4 26 L 19.4 20.6 L 16.6 20.6 L 16.6 26 L 15 26 Z"/><path d="M 23 16 L 29 16 L 29 26 L 27.4 26 L 27.4 17.6 L 24.6 17.6 L 24.6 26 L 23 26 Z"/>',
+
+  solid:
+    '<path d="M 34 7 L 34 29 C 34 30.105 33.105 31 32 31 L 4 31 C 2.895 31 2 30.105 2 29 L 2 7 C 2 5.895 2.895 5 4 5 L 32 5 C 33.105 5 34 5.895 34 7 Z M 7 26 L 13 26 L 13 10 L 7 10 Z M 15 26 L 21 26 L 21 19 L 15 19 Z M 23 26 L 29 26 L 29 16 L 23 16 Z"/>',
+
+  solidAlerted:
+    '<path d="M 34 29 C 34 30.105 33.105 31 32 31 L 4 31 C 2.895 31 2 30.105 2 29 L 2 7 C 2 5.895 2.895 5 4 5 L 21.958 5 L 17.625 12.395 C 16.795 13.601 17.594 15.245 19.064 15.351 C 19.134 15.357 19.201 15.359 19.27 15.357 L 34 15.357 Z M 7 26 L 13 26 L 13 10 L 7 10 Z M 15 26 L 21 26 L 21 19 L 15 19 Z M 23 26 L 29 26 L 29 16 L 23 16 Z"/>',
+
+  solidBadged:
+    '<path d="M 34 12.34 L 34 29 C 34 30.105 33.105 31 32 31 L 4 31 C 2.895 31 2 30.105 2 29 L 2 7 C 2 5.895 2.895 5 4 5 L 22.57 5 C 21.969 9.233 25.05 13.1 29.31 13.46 L 30.32 13.48 C 31.625 13.429 32.895 13.036 34 12.34 Z M 7 26 L 13 26 L 13 10 L 7 10 Z M 15 26 L 21 26 L 21 19 L 15 19 Z M 23 26 L 29 26 L 29 16 L 23 16 Z"/>',
+};
+
+export const barChartIconName = 'bar-chart';
+export const barChartIcon: IconShapeTuple = [barChartIconName, renderIcon(icon)];

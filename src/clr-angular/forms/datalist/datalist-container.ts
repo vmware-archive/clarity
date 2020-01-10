eba399a1da6473d93f9a5bfb0ec95dc8210c62b0
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-01-10 09:34:34 +0200

    fix(datalist): wrong import path for DynamicWrapper
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/clr-angular/forms/datalist/datalist-container.ts
+++ src/clr-angular/forms/datalist/datalist-container.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -7,7 +7,7 @@
 import { Component, ContentChild, Inject } from '@angular/core';
 import { ControlClassService } from '../common/providers/control-class.service';
 import { LayoutService } from '../common/providers/layout.service';
-import { DynamicWrapper } from '../../../clr-angular/utils/host-wrapping/dynamic-wrapper';
+import { DynamicWrapper } from '../../utils/host-wrapping/dynamic-wrapper';
 import { ControlIdService } from '../common/providers/control-id.service';
 import { FocusService } from '../common/providers/focus.service';
 import { IfErrorService } from '../common/if-error/if-error.service';

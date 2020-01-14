661ef351c232722cbdc9913b99a3da69043fedcb
Matt Hippely <mhippely@vmware.com>
2020-01-13 16:06:23 -0800

    fix(datalist): adds optional service
    The [datalist] directive needs to guard against non clarity usage. This adds the optional decorator to the service and guards against its presence before setting any id values. closes #4192
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/clr-angular/forms/datalist/datalist.spec.ts
+++ src/clr-angular/forms/datalist/datalist.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -20,10 +20,20 @@ export class TestDatalistId {
   testId;
 }
 
+@Component({
+  template: `
+    <datalist id="native-datalist">
+        <option value="one">One</option>
+        <option value="two">Two</option>
+    </datalist>
+  `,
+})
+export class TestNativeDatalist {}
+
 type Context = TestContext<ClrDatalist, TestDatalistId>;
 
 export default function(): void {
-  describe('Datalist Element', function() {
+  describe('Clarity Datalist Element', function() {
     let datalistIdService: DatalistIdService;
     spec(ClrDatalist, TestDatalistId, undefined, {
       providers: [DatalistIdService],
@@ -41,6 +51,15 @@ export default function(): void {
       this.detectChanges();
       expect(datalistIdService.id).toEqual(this.clarityElement.id);
       expect(this.clarityElement.id).toEqual('custom-id');
+    });
+  });
+
+  describe('Native Datalist Element', function() {
+    spec(ClrDatalist, TestNativeDatalist);
+
+    it('handles native datalist element without any errors', function() {
+      const nativeDatalist: HTMLElement = document.getElementById('native-datalist');
+      expect(nativeDatalist).toBeTruthy();
     });
   });
 }

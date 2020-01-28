ff0842fc882a52d38bcc6c8489865875e232a119
smathis <smathis@vmware.com>
2020-01-28 09:41:13 -0600

    fix(a11y): a11y adjustments to Clarity action blue color
    • adjusting 3.0 blue update to be accessible against our backgrounds
    • current blue is WCAG AA compliant against all Clarity backgrounds
    • this includes our vertical nav background of #ededed (4.55:1)
    • updating dev app color palette; it was way off
    • updating website color palette; it was also way off
    • updating input group icon color to be accessible against gray background
    • updating datepicker icons to be consistent with input group icons
    • updating stackview triangle to more closely match previous color in new palette
    • adjusting size of datepicker and input-group icons to make glyphs appear less blurry
    • updated header-4 color to preserve its previous color
    • header-4 looked too much like header-5 with new palette
    • verified header-4 for a11y with white text
    • updated gemini files
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/website/src/app/documentation/demos/color/color-palette-functional/functional-color-palettes.ts
+++ src/website/src/app/documentation/demos/color/color-palette-functional/functional-color-palettes.ts
@@ -1,41 +1,42 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
+
 export const LIGHT_THEME_COLOR_PALETTES = [
   {
     legend: 'Backgrounds and borders',
     colors: [
       { weight: '0', value: 'hsl(198, 0%, 100%)', text: 'dark' },
       { weight: '50', value: 'hsl(198, 0%, 98%)', text: 'dark' },
-      { weight: '200', value: 'hsl(198, 0%, 93%)', text: 'dark' },
+      { weight: '200', value: 'hsl(198, 0%, 91%)', text: 'dark' },
       { weight: '400', value: 'hsl(198, 0%, 80%)', text: 'dark' },
-      { weight: '500', value: 'hsl(198, 0%, 60%)', text: 'dark' },
-      { weight: '600', value: 'hsl(198, 0%, 45%)', text: 'light' },
-      { weight: '700', value: 'hsl(198, 0%, 34%)', text: 'light' },
+      { weight: '500', value: 'hsl(198, 0%, 70%)', text: 'dark' },
+      { weight: '600', value: 'hsl(198, 0%, 55%)', text: 'light' },
+      { weight: '700', value: 'hsl(198, 0%, 40%)', text: 'light' },
     ],
   },
   {
     legend: 'Row hover, selection',
     colors: [
-      { weight: '200', value: 'hsl(198, 0%, 93%)', text: 'dark' },
+      { weight: '200', value: 'hsl(198, 0%, 91%)', text: 'dark' },
       { weight: 'n/a', value: 'hsl(201, 29%, 88%) ', text: 'dark' },
     ],
   },
   {
     legend: 'Typography',
     colors: [
-      { weight: '700', value: 'hsl(198, 0%, 34%)', text: 'light' },
-      { weight: '900', value: 'hsl(198, 0%, 19%)', text: 'light' },
+      { weight: '700', value: 'hsl(198, 0%, 40%)', text: 'light' },
+      { weight: '900', value: 'hsl(198, 0%, 20%)', text: 'light' },
       { weight: '1000', value: 'hsl(198, 0%, 0%)', text: 'light' },
     ],
   },
   {
     legend: 'Buttons and app-level alerts',
     colors: [
-      { weight: '600', value: 'hsl(198, 100%, 36%)', text: 'light' },
-      { weight: '700', value: 'hsl(14, 100%, 39%)', text: 'light' },
+      { weight: '600', value: 'hsl(198, 100%, 32%)', text: 'light' },
+      { weight: '700', value: 'hsl(9, 100%, 43%)', text: 'light' },
       { weight: '500', value: 'hsl(93, 67%, 38%)', text: 'dark' },
       { weight: '700', value: 'hsl(93, 100%, 26%)', text: 'light' },
       { weight: '400', value: 'hsl(198, 0%, 80%)', text: 'dark' },
@@ -44,39 +45,38 @@ export const LIGHT_THEME_COLOR_PALETTES = [
   {
     legend: 'Standard alerts',
     colors: [
-      { weight: '50', value: 'hsl(198, 54%, 92%)', text: 'dark' },
+      { weight: '50', value: 'hsl(198, 83%, 94%)', text: 'dark' },
       { weight: '50', value: 'hsl(93, 52%, 88%)', text: 'dark' },
-      { weight: '100', value: 'hsl(14, 83%, 84%)', text: 'dark' },
-      { weight: '100', value: 'hsl(48, 97%, 85%)', text: 'dark' },
+      { weight: '100', value: 'hsl(9, 95%, 92%)', text: 'dark' },
+      { weight: '100', value: 'hsl(48, 100%, 89%)', text: 'dark' },
     ],
   },
   {
     legend: 'Headers',
     colors: [
-      { weight: '900', value: 'hsl(198, 0%, 19%)', text: 'light' },
-      { weight: '1000', value: 'hsl(282, 48%, 14%)', text: 'light' },
-      { weight: '700', value: 'hsl(198, 100%, 34%)', text: 'light' },
-      { weight: '800', value: 'hsl(198, 100%, 27%)', text: 'light' },
-      { weight: '1000', value: 'hsl(198, 100%, 11%)', text: 'light' },
+      { weight: '900', value: 'hsl(198, 0%, 20%)', text: 'light' },
+      { weight: '1000', value: 'hsl(282, 100%, 14%)', text: 'light' },
+      { weight: '700', value: 'hsl(198, 100%, 28%)', text: 'light' },
+      { weight: '800', value: 'hsl(198, 100%, 24%)', text: 'light' },
+      { weight: '1000', value: 'hsl(198, 100%, 15%)', text: 'light' },
     ],
   },
   {
     legend: 'Labels & badges',
     colors: [
-      { weight: '600', value: 'hsl(198, 0%, 45%)', text: 'light' },
-      { weight: '50', value: 'hsl(198, 54%, 92%)', text: 'dark' },
-      { weight: '300', value: 'hsl(198, 57%, 71%)', text: 'dark' },
-      { weight: '600', value: 'hsl(198, 100%, 36%)', text: 'light' },
-      { weight: '800', value: 'hsl(198, 100%, 27%)', text: 'light' },
+      { weight: '600', value: 'hsl(198, 0%, 55%)', text: 'light' },
+      { weight: '50', value: 'hsl(198, 83%, 94%)', text: 'dark' },
+      { weight: '300', value: 'hsl(198, 69%, 69%)', text: 'dark' },
+      { weight: '600', value: 'hsl(198, 100%, 32%)', text: 'light' },
+      { weight: '800', value: 'hsl(198, 100%, 24%)', text: 'light' },
       { weight: '500', value: 'hsl(282, 43%, 54%)', text: 'light' },
-      { weight: '100', value: 'hsl(14, 83%, 84%)', text: 'dark' },
-      { weight: '700', value: 'hsl(14, 100%, 39%)', text: 'light' },
-      { weight: '100', value: 'hsl(48, 100%, 83%)', text: 'dark' },
-      { weight: '300', value: 'hsl(48, 94%, 57%)', text: 'dark' },
-      { weight: '500', value: 'hsl(46, 100%, 45%)', text: 'dark' },
+      { weight: '100', value: 'hsl(9, 95%, 92%)', text: 'dark' },
+      { weight: '700', value: 'hsl(9, 100%, 43%)', text: 'light' },
+      { weight: '100', value: 'hsl(48, 100%, 89%)', text: 'dark' },
+      { weight: '300', value: 'hsl(48, 98%, 72%)', text: 'dark' },
+      { weight: '500', value: 'hsl(48, 95%, 48%)', text: 'dark' },
       { weight: '50', value: 'hsl(93, 52%, 88%)', text: 'dark' },
       { weight: '700', value: 'hsl(93, 100%, 26%)', text: 'light' },
-      { weight: '600', value: 'hsl(198, 0%, 45%)', text: 'light' },
     ],
   },
 ];
@@ -128,6 +128,17 @@ export const DARK_THEME_COLOR_PALETTES = [
     ],
   },
   {
+    legend: 'Headers',
+    colors: [
+      { value: '#404E60', text: 'light' },
+      { value: '#165266', text: 'light' },
+      { value: '#1A4C72', text: 'light' },
+      { value: '#5C3552', text: 'light' },
+      { value: '#3E436A', text: 'light' },
+      { value: '#000000', text: 'light' },
+    ],
+  },
+  {
     legend: 'Labels & badges',
     colors: [
       { value: '#0F6082', text: 'light' },
@@ -139,17 +150,6 @@ export const DARK_THEME_COLOR_PALETTES = [
       { value: '#0079B8', text: 'light' },
       { value: '#FF9C32', text: 'dark' },
       { value: '#89CBDF', text: 'dark' },
-    ],
-  },
-  {
-    legend: 'Headers',
-    colors: [
-      { value: '#404E60', text: 'light' },
-      { value: '#165266', text: 'light' },
-      { value: '#1A4C72', text: 'light' },
-      { value: '#5C3552', text: 'light' },
-      { value: '#3E436A', text: 'light' },
-      { value: '#000000', text: 'light' },
     ],
   },
 ];

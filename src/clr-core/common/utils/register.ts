4cd21c5f37405b6832bda2f074b034ae4e065cc9
smathis <smathis@vmware.com>
2020-01-31 08:38:01 -0600

    fix: adding window check for server-side rendering
    • added check for server-side rendering
    • fixed an issue with the dev-core app icon sets page
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/clr-core/common/utils/register.ts
+++ src/clr-core/common/utils/register.ts
@@ -1,19 +1,25 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
 import curryN from 'ramda/es/curryN';
-import __ from './__';
-import { elementExists } from './exists';
+import { elementExists, existsInWindow, isBrowser } from './exists';
 
-const addElementToRegistry = curryN(3, (tagName: string, elementClass: any, registry = window.customElements) => {
-  if (elementExists(tagName)) {
-    console.warn(`${tagName} has already been registered`);
-  } else {
-    registry.define(tagName, elementClass);
+const addElementToRegistry = curryN(
+  3,
+  (tagName: string, elementClass: any, registry: { define: (a: string, b: any) => {} }) => {
+    if (elementExists(tagName)) {
+      console.warn(`${tagName} has already been registered`);
+    } else {
+      registry.define(tagName, elementClass);
+    }
   }
-});
+);
 
-export const registerElementSafely = addElementToRegistry(__, __, window.customElements);
+export function registerElementSafely(tagName: string, elementClass: any) {
+  if (isBrowser() && existsInWindow(['customElements'])) {
+    addElementToRegistry(tagName, elementClass, window.customElements);
+  }
+}

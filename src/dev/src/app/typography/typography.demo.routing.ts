477d259c34c84f9f660bd5f997c14d049c17e52e
smathis <smathis@vmware.com>
2020-02-12 12:29:09 -0600

    feat: line-height erasers exploration
    • proved out line-height eraser across multiple fonts
    • ratios/measurements change for some fonts when font-weight is changed...
    • ...but none of the fonts we care about
    • code in demos forms foundation for the LHEs Core is delivering
    • added line-height eraser demo to dev-app
    • added font-autopsy demo to dev-app
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/dev/src/app/typography/typography.demo.routing.ts
+++ src/dev/src/app/typography/typography.demo.routing.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -12,6 +12,8 @@ import { TypographyHeadersDemo } from './typography-headers';
 import { TypographyLinksDemo } from './typography-links';
 import { TypographyTextDemo } from './typography-text';
 import { TypographyDemo } from './typography.demo';
+import { TypographyLineHeightDemo } from './typography-line-height';
+import { TypographyFontAutopsyDemo } from './typography-font-autopsy';
 
 const ROUTES: Routes = [
   {
@@ -24,6 +26,8 @@ const ROUTES: Routes = [
       { path: 'typography-text', component: TypographyTextDemo },
       { path: 'typography-links', component: TypographyLinksDemo },
       { path: 'typography-font-char-test', component: TypographyFontCharTestDemo },
+      { path: 'typography-line-height', component: TypographyLineHeightDemo },
+      { path: 'typography-font-autopsy', component: TypographyFontAutopsyDemo },
     ],
   },
 ];

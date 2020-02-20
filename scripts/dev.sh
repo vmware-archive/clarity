5b04f74642cd86630bfe1f4a514df807f959673a
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-02-20 13:50:58 -0600

    chore: remove circleci and use github actions
    GitHub Actions have a larger memory allocation and our jobs have been failing on CircleCI since updating to Angular 9 and Node 12 with out of memory type errors.
    
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-npx netlify deploy --dir=./dist/dev --message="Dev App - $CIRCLE_BRANCH@$CIRCLE_SHA1"
+# This downloads a set of key-value env variables, but blocks any requests that aren't from GitHub Actions
+curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
+# dotenv loads up the env variables into the shell, then deploys through Netlify
+npx dotenv -- npx netlify deploy --dir=./dist/dev --message="Dev App - $GITHUB_REF@$GITHUB_SHA"

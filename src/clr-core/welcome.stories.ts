0a5052226e964f2978b41eb78e05f3ec89255d11
Cory Rylan <crylan@vmware.com>
2020-02-26 15:35:22 -0600

    chore: add additional core docs - add about core - browser support - React wrappers reference
    Co-authored-by: Scott Mathis <smathis@vmware.com>
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/clr-core/welcome.stories.ts
+++ src/clr-core/welcome.stories.ts
@@ -4,7 +4,11 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+import { ClarityIcons } from '@clr/core/icon-shapes';
+import { betaIcon } from '@clr/core/icon-shapes/';
 import { html } from 'lit-html';
+
+ClarityIcons.addIcons(betaIcon);
 
 // todo: use mdx format (peer dep on React) or install code example formatter
 
@@ -20,45 +24,41 @@ export const clarityCore = () => {
   return html`
     <style> 
       .welcome {
+        margin: 0 auto;
+        max-width: 34rem;
         text-align: center;
-        position: relative;
         padding-top: 11vh;
       }
 
       .welcome h1 {
         font-size: 3rem;
         font-weight: 200;
+        margin: 0 0 1.3rem 0;
       }
 
       .welcome h2 {
         font-size: 1rem;
+        margin: 0 0 1.4rem 0;
       }
 
       .welcome img {
         width: 100%;
-        max-width: 8rem;
+        max-width: 5rem;
+        margin: 0 auto 1rem auto;
+        display: block;
       }
 
-      .welcome cds-button {
-        margin-top: 1rem;
+      .welcome p {
+        text-align: left;
+        line-height: 1.1rem;
+        max-width: 550px;
+        margin: 0 auto 0.9rem auto;
       }
     </style>
     <section class="welcome">
       <img src="./assets/images/clarity-logo.svg" alt="Clarity Core" />
-      <h1>Clarity Core</h1>
+      <h1>Clarity Core <clr-icon shape="beta" size="xl" solid status="info" style="transform: translate3d(-0.3em, -0.4em, 0)"></clr-icon></h1>
       <h2>Web Component Implementation of the Clarity Design System</h2>
-
-      <cds-button status="primary">
-        <a href="./?path=/story/documentation-welcome--getting-started">Get Started</a>
-      </cds-button>
-    </section>
-  `;
-};
-
-export const gettingStarted = () => {
-  return html`
-    <section class="doc">
-      <h1>Clarity Core Web Components</h1>
 
       <p>
         Clarity Core is a Web Component implementation of the <a href="https://clarity.design">Clarity Design System</a>.
@@ -66,11 +66,39 @@ export const gettingStarted = () => {
       </p>
 
       <p>
-        Core is currently in a stable but beta state and we are actively looking for feedback from teams.
-        You can read more about Core and its goals in our <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc">Medium blog post</a>.
+        Core is currently in a beta state but is ready to try out.
+        We are actively looking for <a href="https://github.com/vmware/clarity/issues/new/choose">feedback</a>
+        from teams on their experiences. You can read more about Core and its goals
+        in our <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc" target="_blank" rel="noopener">Medium blog post</a>.
       </p>
+      <br />
 
-      <h2>Installing</h2>
+      <cds-button status="primary">
+        <a href="./?path=/story/documentation-welcome--getting-started">Get Started</a>
+      </cds-button>
+      <cds-button action="outline">
+        <a href="https://medium.com/claritydesignsystem/clarity-core-72f6d3a029bc" target="_blank" rel="noopener">Learn More</a>
+      </cds-button>
+    </section>
+    <section>
+      
+    </section>
+  `;
+};
+
+export const gettingStarted = () => {
+  return html`
+    <style>
+      .frameworks {
+        text-align: center;
+      }
+
+      .frameworks cds-button {
+        margin: 1rem 0.2rem 0 0; /* todo: replace with layout system */
+      }
+    </style>
+    <section class="doc">
+      <h1>Installing</h1>
 
       <ul>
         <li>
@@ -109,33 +137,137 @@ font-family: 'Clarity City', 'Avenir Next', 'Helvetica Neue', Arial, sans-serif;
           </code></pre>
         </li>
         <li>
-          <h3>3. Import desired Web Component into your JavaScript or TypeScript</h3>
+          <h3>3. Use Web Component with JavaScript or framework of choice.</h3>
+
           <p>
-            Once you have the global CSS installed, you can start importing and using
-            the Clarity Web Components. To use a component import the component into your
-            JavaScript.
+            Currently Core requires a JavaScript bundler import the required dependencies.
+            Core is compatible with tools such as Webpack, RollupJS, Parcel as well
+            as most Framework CLIs. Additional documentation and examples will be added
+            soon for no build step prototyping.
           </p>
-          <pre><code>import '@clr/core/alert';</code></pre>
-        </li>
-        <li>
-          <h3>4. Use Web Component with JavaScript or framework of choice.</h3>
+
+          <p>
+            To use a component import the component into your JavaScript or TypeScript.
+          </p>
+
           <pre><code>import '@clr/core/alert';</code></pre>
 
           <p>
-            Once imported, the component is registered and ready to use in your HTML.
+            Once imported, the component is automatically registered and ready to use in your HTML.
           </p>
+
+          <cds-button status="success">Hello World</cds-button>
 
           <pre><code>
           ${`
 <cds-button>Hello World</cds-button>
 <script>
   const button = document.querySelector('cds-button');
-  button.status = 'danger';
+  button.status = 'success';
 </script>
           `}
           </code></pre>
         </li>
+        <li>
+          <h3>4. Frameworks</h3>
+          <p>
+            Core works in most JavaScript frameworks. For detailed install steps
+            for your framework see our guides below. More framework guides and
+            demos will be added in the near future.
+          </p>
+
+          <div class="frameworks">
+            <cds-button action="outline">
+              <a href="./?path=/story/documentation-welcome--angular">Angular</a>
+            </cds-button>
+            <cds-button action="outline">
+              <a href="./?path=/story/documentation-welcome--vue">Vue</a>
+            </cds-button>
+            <cds-button action="outline">
+              <a href="./?path=/story/documentation-welcome--react">React</a>
+            </cds-button>
+          </div>
+        </li>
       </ul>
+    </section>
+  `;
+};
+
+export const browserSupport = () => {
+  return html`
+    <style>
+      img {
+        max-width: 30rem;
+        margin: 0 auto;
+        display: block;
+      }
+    </style>
+    <section class="doc">
+      <h1>Browser Support</h1>
+
+      <p>
+        Core currently works across all modern browsers.
+      </p>
+
+      <img alt="Device and Browser Support" src="https://clarity.design/assets/images/get-started/device_support.png">
+
+      <h2>IE11 Support</h2>
+      <p>
+        To support ES5 browsers such as IE11 there are two steps that must be followed.
+      </p>
+
+      <ol>
+        <li>
+          <h3>1. Polyfills</h3>
+          <p>
+            To support Custom Elements and CSS Custom Properties a set of Polyfills
+            must be loaded into IE11.
+          </p>
+          <pre><code>
+npm install @webcomponents/webcomponentsjs
+          </code></pre>
+
+          <pre><code>
+          ${`
+<script src="./node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js"></script>
+<script src="./node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js"></script>
+          `}
+          </code></pre>
+        </li>
+        <li>
+          <h3>2. CSS Custom Properties</h3>
+          <p>
+            Clarity uses CSS Custom Properties to theme and style components.
+            To support IE you will need to run a polyfill and function Core provides
+            for IE11.
+          </p>
+
+          <pre><code>
+import { runCssVarsPolyfill } from '@clr/core';
+
+runCssVarsPolyfill(); // run at application start up or any time after a theme swap
+          </code></pre>
+        </li>
+        <li>
+          <h3>3. Transpile to ES5</h3>
+          <p>
+            Lastly all application code and dependencies must be targeted to transpile
+            down to ES5 code for IE11. Clarity and many other libraries only ship
+            ES2015+ compatible code and allow the consumer to choose their compilation
+            target of choice.
+          </p>
+          <p>
+            Many tools support this out of the box.Please refer to your build system
+            documentation to find out how to target for ES5 clients.
+          </p>
+          <ul>
+            <li><a href="https://angular.io/guide/deployment#differential-loading" target="_blank">angular.io/guide/deployment#differential-loading</a></li>
+            <li><a href="https://cli.vuejs.org/guide/browser-compatibility.html#modern-mode" target="_blank">cli.vuejs.org/guide/browser-compatibility.html#modern-mode</a></li>
+            <li><a href="https://cli.vuejs.org/config/#transpiledependencies" target="_blank">cli.vuejs.org/config/#transpiledependencies</a></li>
+            <li><a href="https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md" target="_blank">github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md</a></li>
+          </ul>
+        </li>
+      </ol>
     </section>
   `;
 };
@@ -144,6 +276,13 @@ export const angular = () => {
   return html`
     <section class="doc">
       <h1>Angular</h1>
+
+      <p>
+        Clarity Core is compatible will most JavaScript frameworks including Angular.
+        You can try out Core in Angular today. To get the best experience building
+        with Clarity we recommend using <a href="https://clarity.design/documentation/get-started">Clarity Angular</a>
+        which provides a suite of feature rich native Angular components.
+      </p>
 
       <p>
         To use Clarity Core with Angular follow the package <a href="/?path=/story/welcome--getting-started">installation instructions</a>.
@@ -204,7 +343,7 @@ import '@clr/core/alert';
 
       <p>
         To set properties on a Web Component use the Vue <code>:property</code> binding syntax.
-        To listen to events use the Angular <code>@event</code> binding syntax.
+        To listen to events use the Vue <code>@event</code> binding syntax.
       </p>
 
       <pre><code>
@@ -218,8 +357,7 @@ Example of a alert web component in Vue
 
 <cds-alert status="info" :closable="true" @closeChange="log">
   Hello World
-</cds-alert>
-      `}
+</cds-alert>`}
       </code></pre>
     </section>
   `;
@@ -256,8 +394,7 @@ Example of a alert web component in Preact
 
       <h2>React</h2>
       <p>
-        Due to React not being compatible with Web Standards such as <a href="https://custom-elements-everywhere.com">custom elements</a>
-        a shim layer must be used. You can try out a <a href="https://stackblitz.com/edit/react-ts-drskpx">early work in progress prototype here</a>.
+      A shim layer <em>(a.k.a wrapper)</em> must be used for web components to work in React applications. This is due to React not being compatible with Web Standards such as <a href="https://custom-elements-everywhere.com">custom elements</a>. You can try out a <a href="https://stackblitz.com/edit/react-ts-drskpx">early work in progress prototype here</a>.
       </p>
     </section>
   `;

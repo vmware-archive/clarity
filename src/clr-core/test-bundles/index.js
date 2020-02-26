8f3a54aab72e506a19824fda11c3a25d3bff9712
Cory Rylan <crylan@vmware.com>
2020-02-26 00:18:24 -0600

    chore: update core bundle tests
    Signed-off-by: Cory Rylan <crylan@vmware.com>

--- src/clr-core/test-bundles/index.js
+++ src/clr-core/test-bundles/index.js
@@ -1,5 +1,7 @@
 import '@clr/core/badge';
+import '@clr/core/icon';
+import { ClarityIcons, userIcon } from '@clr/core/icon-shapes';
 import { CommonStringsService } from '@clr/core';
 
-// todo replace common strings import with two icon imports
+ClarityIcons.addIcons(userIcon);
 console.log(CommonStringsService);

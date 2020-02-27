98c1dd99376d9416cb11b53bb2f4c0b919f00476
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-02-27 13:04:37 -0600

    chore: update version info on website
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- src/website/src/environments/environment.prod.ts
+++ src/website/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 export const environment = {
   production: true,
-  latest: 'v2',
+  latest: 'v3',
   version: 'v2',
-  versions_url: './versions.json',
+  versions_url: 'https://clarity.design/versions.json',
   icons_base_url: '/.netlify/functions/download-icon',
   icons_url: 'https://github.com/vmware/clarity-assets/archive/master.zip',
   sketch_base_url: 'https://github.com/vmware/clarity-assets/raw/master/sketch',

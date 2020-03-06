73c46d25ee238bcbc6e3fa96e090a2d6ab55524f
Gerin Jacob <gerinjacob@gmail.com>
2020-03-06 10:06:16 +0530

    feat(form): add error object to clrIfError context
    Close: #4345
    
    Signed-off-by: Gerin Jacob <gerinjacob@gmail.com>

--- src/clr-angular/forms/common/if-error/if-error.ts
+++ src/clr-angular/forms/common/if-error/if-error.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -52,7 +52,7 @@ export class ClrIfError {
 
   private displayError(invalid: boolean) {
     if (invalid && !this.displayed) {
-      this.container.createEmbeddedView(this.template);
+      this.container.createEmbeddedView(this.template, { error: this.control.getError(this.error) });
       this.displayed = true;
     } else if (!invalid) {
       this.container.clear();

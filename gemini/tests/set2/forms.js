a3d5a89dc78fde050e99257b9fe8824e76e8c1ad
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-03-25 14:33:24 +0200

    feat(input): input groups style changes
      - Include fix for icon and icon placeholder for compact layout
      - Class for handle addons inside input-groups
      - Update on Gemini tests screenshots
      - Fix on input group space between elements
    
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- gemini/tests/set2/forms.js
+++ gemini/tests/set2/forms.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -129,9 +129,9 @@ gemini.suite('forms', child => {
     child
       .setUrl('/forms/input-group')
       .before((actions, find) => {
-        actions.waitForElementToShow('.clr-form', WAIT_TIME);
+        actions.waitForElementToShow('.input-group-example', WAIT_TIME);
       })
-      .setCaptureElements('.clr-form')
+      .setCaptureElements('.input-group-example')
       .capture('default');
   });
 

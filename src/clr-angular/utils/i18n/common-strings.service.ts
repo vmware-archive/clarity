e157430d390baf0ad66e838c2615437bb01911e3
smathis <smathis@vmware.com>
2020-03-25 09:37:39 -0500

    fix(i18n): restore angular common strings service
    • crossing the streams with a service shared b/w core and angular was causing nx and jest to lose their minds
    • much of the problem was due to when typescript or jest would decide to compile an import down to ES5 or not
    • recent discussions have uncovered the potential for more significant divergence in the scope of what we previously felt would be shared services
    • in the future, a shared resource may need to be pulled into angular proper – as opposed to being brought in as a dependency
    • i verified this approach solves breakages in previously breaking apps
    • users still need to use `transformIgnorePatterns` in their jest config to allow babel to compile core, lit, and ramada
    • users will NOT need to override ClrCommonStrings in their spec configs
    • that was a band-aid that was going to break again soon in any case
    
    Tested in:
    ✔︎ Chrome
    
    Fixes: #4094
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/clr-angular/utils/i18n/common-strings.service.ts
+++ src/clr-angular/utils/i18n/common-strings.service.ts
@@ -1,12 +1,43 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
+
 import { Injectable } from '@angular/core';
-import { CommonStringsServiceInternal } from '@clr/core/common';
+import { commonStringsDefault, ClrCommonStrings } from '@clr/core/common';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClrCommonStringsService extends CommonStringsServiceInternal {}
+export class ClrCommonStringsService {
+  private _strings = commonStringsDefault;
+
+  /**
+   * Allows you to pass in new overrides for localization
+   */
+  localize(overrides: Partial<ClrCommonStrings>) {
+    this._strings = { ...this._strings, ...overrides };
+  }
+
+  /**
+   * Access to all of the keys as strings
+   */
+  get keys(): Readonly<ClrCommonStrings> {
+    return this._strings;
+  }
+
+  /**
+   * Parse a string with a set of tokens to replace
+   */
+  parse(source: string, tokens: { [key: string]: string } = {}) {
+    const names = Object.keys(tokens);
+    let output = source;
+    if (names.length) {
+      names.forEach(name => {
+        output = output.replace(`{${name}}`, tokens[name]);
+      });
+    }
+    return output;
+  }
+}

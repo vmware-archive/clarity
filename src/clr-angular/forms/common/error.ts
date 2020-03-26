362047f8c55f2ed3f8123f5fc49e73097a48fb97
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-03-26 16:15:11 +0200

    fix(a11y): prevent clr-control-error from announcing unwanted messages
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/clr-angular/forms/common/error.ts
+++ src/clr-angular/forms/common/error.ts
@@ -7,6 +7,7 @@
 import { Component, Optional, ElementRef, AfterViewInit } from '@angular/core';
 import { ControlIdService } from './providers/control-id.service';
 import { ClrAriaLiveService } from '../../utils/a11y/aria-live.service';
+import { ControlClassService } from './providers/control-class.service';
 
 @Component({
   providers: [ClrAriaLiveService],
@@ -22,12 +23,28 @@ import { ClrAriaLiveService } from '../../utils/a11y/aria-live.service';
 export class ClrControlError implements AfterViewInit {
   constructor(
     @Optional() public controlIdService: ControlIdService,
+    @Optional() public controlClassService: ControlClassService,
     private ariaLiveService: ClrAriaLiveService,
     private el: ElementRef
   ) {}
 
+  /** @deprecated since 3.0, remove in 4.0 - ariaLiveService */
   ngAfterViewInit() {
-    /** @deprecated since 3.0, remove in 4.0 */
-    this.ariaLiveService.announce(this.el.nativeElement);
+    /**
+     * The way we render elements inside the `clr-control-container make this
+     * component announce itself without been visible on the screen.
+     *
+     * The check below try to guess is clr-controll-error used in some of the
+     * cases mention above and prevent us from announcing without the need of that.
+     *
+     * This change won't create breaking change - but will make aria live announcment
+     * work only when the component is used outside forms or some of the components.
+     *
+     * This is temporary solution - until better is found.
+     *
+     */
+    if (this.controlClassService === null) {
+      this.ariaLiveService.announce(this.el.nativeElement);
+    }
   }
 }

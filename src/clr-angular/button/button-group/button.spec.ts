809f25909399f5d819e46f5423b2aec05d133aae
smathis <smathis@vmware.com>
2020-04-02 10:22:33 -0500

    fix: id="null" for clr-buttons
    • backport of PR #4352
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- src/clr-angular/button/button-group/button.spec.ts
+++ src/clr-angular/button/button-group/button.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -18,7 +18,6 @@ import { ClrButtonGroupModule } from './button-group.module';
   template: `
         <clr-button
             #button1
-            id="button1"
             type="button"
             name="button1"
             id="button1"
@@ -261,8 +260,8 @@ export default function(): void {
       });
 
       it('sets the id correctly', () => {
-        expect(buttons[0].id).toBe('null');
-        expect(buttons[1].id).toBe('null');
+        expect(buttons[0].id).toBe('');
+        expect(buttons[1].id).toBe('');
         expect(buttons[2].id).toBe('button3');
       });
 
--- src/clr-angular/button/button-group/button.ts
+++ src/clr-angular/button/button-group/button.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -20,7 +20,7 @@ import { ButtonInGroupService } from '../providers/button-in-group.service';
                 [attr.type]="type"
                 [attr.name]="name"
                 [attr.disabled]="disabled"
-                [id]="id">
+                [attr.id]="id">
                 <span class="spinner spinner-inline" *ngIf="loading"></span>
                 <ng-content></ng-content>
             </button>

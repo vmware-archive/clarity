290b9ec03af29f8679efff6f3e558cff76dd3057
Bram Meerten <bram.mee@gmail.com>
2020-04-14 20:24:04 +0200

    fix: don't update view after opening and immediately closing a popover
    Close: #4430
    
    Signed-off-by: Bram Meerten <bram.mee@gmail.com>

--- src/clr-angular/utils/popover/popover-content.spec.ts
+++ src/clr-angular/utils/popover/popover-content.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  *
@@ -170,6 +170,18 @@ export default function(): void {
         expect(testElement.style.top).toMatch(/\d+px/);
         expect(testElement.style.left).toMatch(/\d+px/);
       });
+
+      it(
+        'does not fail when the popup view is immediately destroyed',
+        fakeAsync(function(this: Context) {
+          this.testComponent.openState = true;
+          this.fixture.detectChanges();
+          this.testComponent.openState = false;
+          this.fixture.detectChanges();
+
+          expect(tick).not.toThrowAnyError();
+        })
+      );
     });
   });
 }
--- src/clr-angular/utils/popover/popover-content.ts
+++ src/clr-angular/utils/popover/popover-content.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  *
@@ -75,14 +75,18 @@ export class ClrPopoverContent implements AfterContentChecked, OnDestroy {
         this.shouldRealign = true;
         // Avoid flickering on initialization, caused by the asynchronous nature of the
         // check-collector pattern.
-        this.renderer.setStyle(this.view.rootNodes[0], 'opacity', '0');
+        if (this.view) {
+          this.renderer.setStyle(this.view.rootNodes[0], 'opacity', '0');
+        }
       }),
       // Here we collect subsequent synchronously received content-check events and only take action
       // at the end of the cycle. See below for details on the check-collector pattern.
       this.checkCollector.pipe(debounceTime(0)).subscribe(() => {
         this.alignContent();
         this.shouldRealign = false;
-        this.renderer.setStyle(this.view.rootNodes[0], 'opacity', '1');
+        if (this.view) {
+          this.renderer.setStyle(this.view.rootNodes[0], 'opacity', '1');
+        }
       })
     );
   }

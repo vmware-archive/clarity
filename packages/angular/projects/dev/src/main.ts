2b693945d3b82bb0d67c2b641f3c00d9e6836ff2
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-05-07 12:33:43 -0500

    chore: move dev app files
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- packages/angular/projects/dev/src/main.ts
+++ packages/angular/projects/dev/src/main.ts
@@ -1,12 +1,17 @@
+import '@clr/icons';
+import '@clr/icons/shapes/essential-shapes';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { runCssVarsPolyfill } from '@clr/core';
 
 if (environment.production) {
   enableProdMode();
 }
+
+runCssVarsPolyfill();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

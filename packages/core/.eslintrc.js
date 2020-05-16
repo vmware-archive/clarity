065bc5cea1c4f073431a2280652d98c5952d6b13
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2020-05-15 21:54:42 -0700

    style: replace stylelint.json with js and format eslintrc.js
    Resolve #4614. No rules changes. Add tsconfigRootDir for eslint.rc
    
    Signed-off-by: Alex Malitsky <1457274+amalitsky@users.noreply.github.com>

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,33 +1,43 @@
-module.exports = {
-  extends: '../../.eslintrc.js',
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-    sourceType: 'module',
-  },
-  // Only adding rules that override the defaults or enforce new standards
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off', // TOO MUCH WORK AT THE MOMENT
-    '@typescript-eslint/no-explicit-any': 'off', // Would LOVE to turn this on
-    '@typescript-eslint/no-use-before-define': 'off', // cause slow analysis on TS files with Storybook https://github.com/typescript-eslint/typescript-eslint/issues/1856
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        types: {
-          Array: 'Use [] instead.',
-          Object: 'Use {} instead.',
-          Boolean: 'Use `boolean` instead.',
-          Number: 'Use `number` instead.',
-          String: 'Use `string` instead.',
-        },
-      },
-    ],
-    '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
-    'no-restricted-imports': [
-      'error',
-      {
-        paths: ['ramda', 'rxjs', 'rxjs/operators'],
-        patterns: ['lit-element/*', '@angular/*'],
-      },
-    ],
-  },
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
+const bannedTSTypes = {
+  Array: 'Use [] instead.',
+  Object: 'Use {} instead.',
+  Boolean: 'Use `boolean` instead.',
+  Number: 'Use `number` instead.',
+  String: 'Use `string` instead.',
 };
+
+// only adding rules that override the defaults or enforce new standards
+const rules = {
+  '@typescript-eslint/explicit-function-return-type': OFF, // too much work at the moment
+  '@typescript-eslint/no-explicit-any': OFF, // would LOVE to turn this on
+  // cause slow analysis on TS files with Storybook, see
+  // https://github.com/typescript-eslint/typescript-eslint/issues/1856
+  '@typescript-eslint/no-use-before-define': OFF,
+  '@typescript-eslint/ban-types': [ERROR, { types: bannedTSTypes }],
+  '@typescript-eslint/explicit-member-accessibility': [ERROR, { accessibility: 'no-public' }],
+  'no-restricted-imports': [
+    ERROR,
+    {
+      paths: ['ramda', 'rxjs', 'rxjs/operators'],
+      patterns: ['lit-element/*', '@angular/*'],
+    },
+  ],
+};
+
+const parserOptions = {
+  project: 'tsconfig.eslint.json',
+  tsconfigRootDir: __dirname,
+  sourceType: 'module',
+};
+
+const config = {
+  extends: '../../.eslintrc.js',
+  parserOptions,
+  rules,
+};
+
+module.exports = config;
---
+++ packages/core/stylelint.config.js
@@ -0,0 +1,46 @@
+const rules = {
+  'sh-waqar/declaration-use-variable': [
+    [
+      'color',
+      {
+        ignoreValues: ['inherit', 'red', 'blue', '/^getSassButtonColor/'],
+      },
+    ],
+  ],
+  'meowtec/no-px': [
+    true,
+    {
+      ignore: ['1px', '-1px'],
+    },
+  ],
+  'color-no-hex': true,
+  'no-descending-specificity': null,
+  'no-duplicate-selectors': null,
+  'at-rule-no-unknown': null,
+  'scss/at-rule-no-unknown': true,
+  'selector-type-no-unknown': [
+    true,
+    {
+      ignoreTypes: ['/^cds-/', '/^_/'],
+    },
+  ],
+  'max-nesting-depth': 3,
+  'rule-empty-line-before': [
+    'always-multi-line',
+    {
+      except: ['first-nested'],
+      ignore: ['after-comment'],
+    },
+  ],
+};
+
+const config = {
+  extends: 'stylelint-config-recommended',
+  defaultSeverity: 'error',
+  syntax: 'scss',
+  plugins: ['stylelint-scss', 'stylelint-declaration-use-variable', 'stylelint-no-px'],
+  ignoreFiles: ['./src/styles/tokens/generated/*.scss'],
+  rules,
+};
+
+module.exports = config;

3e65bbc48c7a99df1e4db749e3658ed037f94b22
Matt Hippely <mhippely@vmware.com>
2020-06-10 10:22:19 -0700

    docs: adds final ng stories
    - basic ng grid
    - basic ng list
    - basic ng login
    
    Had to also add some override styles to make the grid work in the storybook preview.
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- packages/angular/src/stories/login/login.stories.ts
+++ packages/angular/src/stories/login/login.stories.ts
@@ -4,15 +4,31 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
+import { moduleMetadata } from '@storybook/angular';
+import { ClarityModule } from '@clr/angular';
+
 const basicTemplate = require('!!raw-loader!./basic.html'); // eslint-disable-line
 
 export default {
   title: 'Login',
+  decorators: [
+    moduleMetadata({
+      imports: [ClarityModule],
+    }),
+  ],
 };
 
 export const Basic = () => {
+  const form = {
+    type: 'local',
+    username: '',
+    password: '',
+    rememberMe: false,
+  };
+
   return {
     title: 'Basic',
     template: basicTemplate.default,
+    props: { form },
   };
 };

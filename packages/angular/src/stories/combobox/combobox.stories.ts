98b6521fd9f654232429fd42a700a7870ded107e
Bogdan Bogdanov <bbogdanov@vmware.com>
2020-07-31 16:02:29 +0300

    docs: add combobox basic story
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

---
+++ packages/angular/src/stories/combobox/combobox.stories.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016-2020 VMware, Inc.
+ * All Rights ReserveThis software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+import { moduleMetadata } from '@storybook/angular';
+import { boolean } from '@storybook/addon-knobs';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const basicTemplate = require('!!raw-loader!./basic.html'); // eslint-disable-line
+
+export default {
+  title: 'Combobox',
+  decorators: [
+    moduleMetadata({
+      imports: [BrowserAnimationsModule, ClarityModule],
+    }),
+  ],
+  props: {
+    clrMulti: false,
+  },
+};
+
+export const Basic = () => {
+  return {
+    title: 'Basic',
+    template: basicTemplate.default,
+    props: {
+      selected: null,
+      disabled: false,
+    },
+  };
+};

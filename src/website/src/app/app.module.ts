f7e5ba9eb1a092a55a6fc44ea4b20778f8842df9
Jeremy Wilken <gnomation@gnomeontherun.com>
2020-08-13 17:34:30 -0500

    docs: revamp docs for subdomain
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- src/website/src/app/app.module.ts
+++ src/website/src/app/app.module.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -10,7 +10,6 @@ import { ClarityModule } from '@clr/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 import { DocumentationModule } from './documentation/documentation.module';
@@ -18,7 +17,7 @@ import { UtilsModule } from './utils/utils.module';
 import { SkipLinkComponent } from './utils/skip-link.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PageNotFoundComponent, SkipLinkComponent],
+  declarations: [AppComponent, PageNotFoundComponent, SkipLinkComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'clarity' }),
     BrowserAnimationsModule,

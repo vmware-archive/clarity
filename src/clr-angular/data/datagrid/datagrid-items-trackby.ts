04595db8d54cf075ad00c28aa10e5289306913ca
Bogdan Bogdanov <bbogdanov@vmware.com>
2020-09-02 20:21:55 +0300

    Revert "fix(datagrid): ngForTrackBy selector of datagrid items to be more specific"
    This reverts commit e2d6882b916630d2ba9f434d6bcafc094ec2ca8a.
    
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- src/clr-angular/data/datagrid/datagrid-items-trackby.ts
+++ src/clr-angular/data/datagrid/datagrid-items-trackby.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2018 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -8,12 +8,12 @@ import { Directive, Input, Optional, TrackByFunction } from '@angular/core';
 import { Items } from './providers/items';
 
 @Directive({
-  selector: '[clrDgItemsTrackBy]',
+  selector: '[ngForTrackBy]',
 })
 export class ClrDatagridItemsTrackBy<T = any> {
   constructor(@Optional() private _items: Items<T>) {}
 
-  @Input('clrDgItemsTrackBy')
+  @Input('ngForTrackBy')
   set trackBy(value: TrackByFunction<T>) {
     if (this._items) {
       this._items.trackBy = value;

80f6e3450d274cdd068c5bc2f2ff0750f3baf290
Bogdan Bogdanov <bbogdanov@vmware.com>
2020-09-14 18:28:53 +0300

    docs: add component overview page
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

---
+++ apps/website/.vuepress/theme/global-components/ComponentsOverview.vue
@@ -0,0 +1,39 @@
+<template>
+  <section cds-layout="m-y:xl grid gap:md cols@sm:12 cols@md:6 cols@lg:4 align:vertical-stretch">
+    <ItemOverview
+      v-for="component in componentList"
+      :title="component.title"
+      :path="component.path"
+      :target="component.target"
+      :isNew="component.isNew"
+    >
+      <template>
+        <img cds-layout="fill" :src="component.name | adjustToSvgUrl" :alt="component.title + ' visual example'" />
+      </template>
+    </ItemOverview>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ComponentsOverview',
+  filters: {
+    adjustToSvgUrl: componentName => {
+      return `/images/components/overview/${componentName}.svg`;
+    },
+  },
+  computed: {
+    componentList: function () {
+      if (this.$site && this.$site.themeConfig && this.$site.themeConfig.sidebar) {
+        const componentsSection = this.$site.themeConfig.sidebar.find(x => x.title === 'Components');
+
+        if (componentsSection) {
+          return componentsSection.children.slice(1);
+        }
+      }
+
+      return [];
+    },
+  },
+};
+</script>

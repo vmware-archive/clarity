fc1e250db3d8eabb378837a6d2bf0ea93293d5f1
Matt Hippely <mhippely@vmware.com>
2020-10-06 16:36:14 -0700

    build: test building website with netlify
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -10,4 +10,7 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 find . -type f -name "*.html" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
 find . -type f -name "*.js" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
 
-node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
+# Deploy a preview that can be promoted to production when we are ready
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "clarity.design"
+# Deploy a version to https://next.clarity.design as 'production'
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "next.clarity.design"

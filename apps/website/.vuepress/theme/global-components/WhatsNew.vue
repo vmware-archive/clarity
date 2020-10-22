72b92ac462441d68f6b3d64962c5586b2b83a44a
Matt Hippely <mhippely@vmware.com>
2020-10-22 14:16:09 -0700

    docs: adds `expanded` line-height for cds-text tokens to website
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- apps/website/.vuepress/theme/global-components/WhatsNew.vue
+++ apps/website/.vuepress/theme/global-components/WhatsNew.vue
@@ -1,6 +1,6 @@
 <template>
   <div cds-layout="vertical">
-    <h1 cds-text="title medium">v{{ data.version }} is now available!</h1>
+    <h1 cds-text="title medium expanded">v{{ data.version }} is now available!</h1>
     <p v-if="data.feat.length > 0" cds-text="message medium" cds-layout="m-t:md">
       <span v-if="data.feat[0]">{{ data.feat[0].title }}</span>
       <span v-if="data.feat[1]">, {{ data.feat[1].title }}</span>

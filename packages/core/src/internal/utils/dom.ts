7f6961367622d1f9b924bbe5225e57fb44dca9a7
Scott Mathis <smathis@vmware.com>
2020-10-30 13:04:01 -0500

    feat(build): update core and react to typescript 4
    • fixed non-null assertion issue that tslint was warning about
    • fixed issue with use of delete on focusTrap object
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- packages/core/src/internal/utils/dom.ts
+++ packages/core/src/internal/utils/dom.ts
@@ -111,7 +111,9 @@ export function isVisible(element: HTMLElement) {
 
 export function spanWrapper(nodeList: NodeListOf<ChildNode>): void {
   Array.from(nodeList)
-    .filter(node => node.textContent!.trim().length > 0)
+    .filter(node => {
+      return node.textContent && node.textContent.trim().length > 0;
+    })
     .forEach(node => {
       if (node.parentElement && node.textContent !== null && node.nodeType === 3) {
         const spanWrapper = document.createElement('span');

dd7db48e94a495f2b0cfa656310c618a716f04ae
Jérémie Astori <jeremie@astori.fr>
2020-11-29 23:30:30 -0500

    feat(icons): export icon names in entrypoint of @clr/core/icon
    docs(icons): document how to use type checked icon names
    
    Signed-off-by: Jérémie Astori <jeremie@astori.fr>

--- packages/react/src/icon/index.tsx
+++ packages/react/src/icon/index.tsx
@@ -3,4 +3,16 @@ import '@cds/core/icon/register';
 import { createReactComponent } from '../converter/react-wrapper';
 
 type CdsIconType = Icon;
+/**
+ * If using JSX or TSX, import the icon name from `@clr/core/icon` and include it in the `shape` prop to improve type safety:
+ *
+ * ```tsx
+ * import { ClarityIcons, userIcon, userIconName } from '@clr/core/icon';
+ * import { CdsIcon } from '@clr/react/icon';
+ *
+ * ClarityIcons.addIcons(userIcon);
+ *
+ * <CdsIcon shape={userIconName} />
+ * ```
+ */
 export class CdsIcon extends createReactComponent<CdsIconType>('cds-icon') {}

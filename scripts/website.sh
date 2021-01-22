5fb377c5d26016232cdbcd98da958edae3186b92
Matt Hippely <mhippely@vmware.com>
2021-01-21 16:24:32 -0800

    build: update v4 for subdomain
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -4,10 +4,5 @@
 curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
-# update website for production storybook urls
-# NOTE: this leaves core untouched b/c vuepress code uses the //localhost:6006 url and we are
-# replacing the '/localhost:6006' part with 'storybook/angular'
-find . -type f -name "*.html" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
-find . -type f -name "*.js" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
-
-node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA"
+# Deploy a preview that can be promoted to v4.clarity.design when we are ready
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "v4.clarity.design"

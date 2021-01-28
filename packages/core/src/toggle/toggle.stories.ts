f76503a0aa3d74871cbd824ff0df05b9f6c735fc
Scott Mathis <smathis@vmware.com>
2021-01-28 09:13:37 -0600

    fix: website toggle demo
    • added a `start:dev` command to package.json to make it easier to run off clean cache to test locally
    • updated success status website demo and core demo to better illustrate the toggle component
    
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- packages/core/src/toggle/toggle.stories.ts
+++ packages/core/src/toggle/toggle.stories.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -73,7 +73,7 @@ export function status() {
 
       <cds-toggle status="success">
         <label>success</label>
-        <input type="checkbox" />
+        <input type="checkbox" checked />
         <cds-control-message status="success">success text</cds-control-message>
       </cds-toggle>
     </div>

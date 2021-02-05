928e8287f680cbbcbdce00929bba0a5ef102c6cf
Alex Malitsky <github@amalitsky.com>
2021-02-05 10:36:50 -0800

    docs: add table view for charts
    Signed-off-by: Alex Malitsky <github@amalitsky.com>

---
+++ apps/website/.vuepress/theme/global-components/doc/charts/HighChartCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div cds-layout="vertical">
+    <div cds-layout="horizontal align:right m-b:md">
+      <cds-button action="flat" cds-layout="align:left" @click="toggleView" :disabled="darkTheme">
+        {{ viewToggleButtonLabel }}
+      </cds-button>
+      <cds-select layout="compact" control-width="shrink">
+        <label>Select Chart Theme</label>
+        <select v-model="themeId" :disabled="tableView">
+          <option v-for="option of themeOptions" :key="option.value" :value="option.value">{{ option.label }}</option>
+        </select>
+      </cds-select>
+      <cds-toggle cds-layout="m-l:lg">
+        <label>Dark Theme</label>
+        <input type="checkbox" v-model="darkTheme" :disabled="tableView" />
+      </cds-toggle>
+    </div>
+    <div class="card" cds-layout="horizontal p:none" :cds-theme="darkThemeAttributeValue">
+      <slot :themeId="themeId" :darkTheme="darkTheme" :tableView="tableView"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * @file
+ * HighChart wrapper component with charts theme, light/dark theme and table/chart view selectors.
+ */
+
+const themeOptions = [];
+
+for (let i = 1; i <= 12; i++) {
+  themeOptions.push({
+    value: i,
+    label: `Theme ${i}`,
+  });
+}
+
+export default {
+  name: 'HighChartCard',
+  data() {
+    return {
+      themeId: 12,
+      darkTheme: false, // only one of these can be true
+      tableView: false,
+      themeOptions,
+    };
+  },
+  computed: {
+    darkThemeAttributeValue() {
+      return this.darkTheme ? 'dark' : null;
+    },
+    viewToggleButtonLabel() {
+      return this.tableView ? 'Chart View' : 'Table View';
+    },
+  },
+  methods: {
+    toggleView() {
+      this.tableView = !this.tableView;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  // to avoid content shift upon chart rendering
+  min-height: calc(var(--cds-global-space-13) * 6); // 20rem
+}
+</style>

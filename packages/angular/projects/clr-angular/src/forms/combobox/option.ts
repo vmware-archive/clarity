7b89b05995379f07b3cbdae7a5fae45be3255008
Ivan Donchev <idonchev@vmware.com>
2021-02-17 15:58:57 +0200

    fix: combobox-set id, double esc event, click-to-close, focus-error
    closes #5565, closes #5344, closes #5185, closes #5553
    Signed-off-by: Ivan Donchev <idonchev@vmware.com>

--- packages/angular/projects/clr-angular/src/forms/combobox/option.ts
+++ packages/angular/projects/clr-angular/src/forms/combobox/option.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -77,6 +77,11 @@ export class ClrOption<T> implements OnInit {
     } else {
       this.optionSelectionService.select(this.value);
     }
+    // As the popover stays open in multi-select mode now, we have to take focus back to the input
+    // This way we achieve two things:
+    // - do not lose focus
+    // - we're still able to use onBlur for "outside-click" handling
+    this.focusHandler.focusInput();
   }
 
   @HostBinding('class.clr-focus')

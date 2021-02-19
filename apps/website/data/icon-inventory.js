30f0f1bdd244722d9735a5c254c353fbf1c46fc0
Jeremy Wilken <gnomation@gnomeontherun.com>
2021-02-19 17:36:11 -0600

    docs: add version info for icons
    This adds a tag for each icon added to Clarity after v1, defaulting to v1 for the rest.
    
    closes #1808
    
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- apps/website/data/icon-inventory.js
+++ apps/website/data/icon-inventory.js
@@ -143,4 +143,55 @@ export default {
     ...arrAliasesToObjAliases(technologyAliases),
     ...arrAliasesToObjAliases(chartAliases),
   },
+  latest: 'v5.0.0',
+  isNew: function (shape) {
+    return this.versionMap[this.latest].indexOf(shape) > -1;
+  },
+  versionMap: {
+    // In the future, you can add a new icons to define what version they were added.
+    // Volume 13 and before were added before v1, and we just don't care to track that far back.
+    'v1.1.0': [
+      'bell-curve',
+      'fish',
+      'form',
+      'fuel',
+      'snowflake',
+      'tabke',
+      'dot-circle',
+      'volume',
+      'crosshairs',
+      'capacitor',
+      'squid',
+      'inductor',
+      'resistor',
+    ], // vol 14 https://github.com/vmware/clarity/pull/3099
+    'v2.1.0': [
+      'text-edit',
+      'highlighter',
+      'indent',
+      'outdent',
+      'strikethrough',
+      'subscript',
+      'superscript',
+      'storage-adapter',
+      'node-group',
+      'nodes',
+      'namespace',
+      'node',
+      'pod',
+      'beta',
+    ], // vol 15 https://github.com/vmware/clarity/pull/3539
+    'v3.0.0': ['first-aid', 'crown', 'hashtag'], // vol 16 https://github.com/vmware/clarity/pull/3965
+    'v4.0.0': [
+      'employee',
+      'employee-group',
+      'factory',
+      'color-palette',
+      'animation',
+      'ci-cd',
+      'file-share2',
+      'on-holiday',
+    ], // vol 17 https://github.com/vmware/clarity/pull/4218
+    'v5.0.0': ['announcement', 'birthday-cake', 'pdf-file', 'script-execute', 'script-schedule', 'xls-file'], // vol 18 https://github.com/vmware/clarity/issues/4839
+  },
 };

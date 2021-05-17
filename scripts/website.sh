ad4a6a25e777be5e22bc1d3741fb9f25a70c16b3
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2021-05-17 16:46:25 +0300

    build: output the netlify information into json format
    Netlify deploy command will create json files into `./logs` folder with
    the latest deploy information. This will include urls and netlify
    preview id. This will be most helpful for other commands that need to
    know the deployed address or ID.
    
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p logs
+
 # This downloads a set of key-value env variables, but blocks any requests that aren't from GitHub Actions
 curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
 # dotenv loads up the env variables into the shell, then deploys through Netlify
@@ -11,6 +13,12 @@ find . -type f -name "*.html" -print0 -path ./website/storybook -prune | xargs -
 find . -type f -name "*.js" -print0 -path ./website/storybook -prune | xargs -0 perl -pi -e 's/\/localhost:6006\//storybook\/angular\/index.html/g'
 
 # Deploy a preview that can be promoted to production when we are ready
-node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "clarity.design"
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --json --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "clarity.design" > ./logs/clarity.design.netlify.json
 # Deploy a version to https://next.clarity.design as 'production'
-node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "next.clarity.design"
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --json --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "next.clarity.design" > ./logs/next.clarity.design.netlify.json
+
+echo "Clarity Design Netlify"
+cat ./logs/clarity.design.netlify.json
+
+echo "Clarity Next Design Netlify"
+cat ./logs/next.clarity.design.netlify.json

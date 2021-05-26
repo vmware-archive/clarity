09eedcee390ca1e61a5d411a76c81ed85eb08cb2
stsogoo <stsogoo@vmware.com>
2021-05-26 12:36:30 -0700

    fix: eslint fix
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- eslint/eslint-plugin-clarity/rules/no-barrel-imports.js
+++ eslint/eslint-plugin-clarity/rules/no-barrel-imports.js
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-// eslint-disable-next-line
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { resolve, extname, basename } = require('path');
-// eslint-disable-next-line
+
 const { statSync } = require('fs');
 
 module.exports = {

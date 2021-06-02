ee8abe294c601e58b152a6442d3eb0f3b6717902
stsogoo <stsogoo@vmware.com>
2021-06-02 15:14:24 -0700

    build: netlify build
    Signed-off-by: stsogoo <stsogoo@vmware.com>

---
+++ scripts/netlify-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2016-2020 VMware, Inc.
+# All Rights ReserveThis software is released under MIT license.
+# The full license information can be found in LICENSE in the root directory of this project.
+#
+
+echo "Clarity Netlify Build Script"
+exit 0

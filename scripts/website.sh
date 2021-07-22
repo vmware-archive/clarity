f04cb111405dde92205530333541893b8c875be1
Matt Hippely <mhippely@vmware.com>
2021-07-22 10:12:34 -0700

    chore: bump to 12.0.0
    This also includes the website news updates.
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -7,5 +7,5 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
 # Deploy a preview that can be promoted to production when we are ready
-node -r dotenv/config -- ./node_modules/.bin/netlify deploy --json --dir=./dist/website --message="Angualr Branch - Website - $GITHUB_REF@$GITHUB_SHA" --site "angular.clarity.design" > ./logs/angular.clarity.design.netlify.json
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --json --dir=./dist/website --message="Angular Branch - Website - $GITHUB_REF@$GITHUB_SHA" --site "angular.clarity.design" > ./logs/angular.clarity.design.netlify.json
 cat ./logs/angular.clarity.design.netlify.json

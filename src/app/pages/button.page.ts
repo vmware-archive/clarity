093791fbc302c149938508982b8d31cd61b2b983
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-07-23 12:44:27 +0300

    feat(adoption:button): adding buttons examples
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- src/app/pages/button.page.ts
+++ src/app/pages/button.page.ts
@@ -5,12 +5,150 @@
  */
 
 import { Component } from '@angular/core';
+import { DemoTabData } from '../components/demo.component';
 
 @Component({
   selector: 'app-button',
   template: `
     <h1>Button</h1>
-    <not-ready name="Button"></not-ready>
+
+    <demo [tabs]="demo1">
+      <h3>Basic</h3>
+    </demo>
+
+    <demo [tabs]="demo2">
+      <h3>All solid buttons</h3>
+    </demo>
+
+    <demo [tabs]="demo3">
+      <h3>All outline buttons</h3>
+    </demo>
+
+    <demo [tabs]="demo4">
+      <h3>Inverse</h3>
+    </demo>
+
+    <demo [tabs]="demo5">
+      <h3>Icon buttons</h3>
+    </demo>
+
+    <demo [tabs]="demo6">
+      <h3>Loading buttons</h3>
+    </demo>
   `,
 })
-export class ButtonPage {}
+export class ButtonPage {
+  demo1: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'button/button.1.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'button/button.1.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo2: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'button/button.2.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'button/button.2.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo3: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'button/button.3.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'button/button.3.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo4: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'button/button.4.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'button/button.4.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo5: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'button/button.5.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'button/button.5.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo6: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'button/button.6.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'button/button.6.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+}

e71d55b6c8b3d15e956e5061be1e169c40d52985
stsogoo <stsogoo@vmware.com>
2021-07-25 20:56:38 -0700

    build: remove clr/angular
    Signed-off-by: stsogoo <stsogoo@vmware.com>

--- packages/angular/projects/clr-angular/src/utils/focus/focus-on-view-init/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
- * This software is released under MIT license.
- * The full license information can be found in LICENSE in the root directory of this project.
- */
-
-export * from './focus-on-view-init';
-export { FOCUS_ON_VIEW_INIT } from './focus-on-view-init.provider';
-export * from './focus-on-view-init.module';

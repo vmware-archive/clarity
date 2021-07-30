f0c63268f864f77f4a90efe184a3794d91a3d349
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-07-30 14:50:54 +0300

    feat(adoption): add input demos
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- src/app/pages/input.page.ts
+++ src/app/pages/input.page.ts
@@ -5,12 +5,104 @@
  */
 
 import { Component } from '@angular/core';
+import { DemoTabData } from '../components/demo.component';
 
 @Component({
   selector: 'app-input',
   template: `
     <h1>Input</h1>
-    <not-ready name="Input"></not-ready>
+
+    <demo [tabs]="demo1">
+      <h3>Basic</h3>
+    </demo>
+
+    <demo [tabs]="demo2">
+      <h3>Input with label</h3>
+    </demo>
+
+    <demo [tabs]="demo3">
+      <h3>Input with label</h3>
+    </demo>
+
+    <demo [tabs]="demo4">
+      <h3>All type of inputs</h3>
+    </demo>
   `,
 })
-export class InputPage {}
+export class InputPage {
+  demo1: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'input/input.1.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'input/input.1.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo2: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'input/input.2.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'input/input.2.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo3: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'input/input.3.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'input/input.3.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo4: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'input/input.4.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'input/input.4.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+}

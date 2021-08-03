4fe4ce280896456dd9a1baf8998454457335dcad
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2021-08-03 10:13:13 +0300

    feat(adoption:modal) adding modal example
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/app/pages/modal.page.ts
+++ src/app/pages/modal.page.ts
@@ -5,12 +5,66 @@
  */
 
 import { Component } from '@angular/core';
+import { DemoTabData } from '../components/demo.component';
 
 @Component({
   selector: 'app-modal',
   template: `
     <h1>Modal</h1>
-    <not-ready name="Modal"></not-ready>
+    <app-eslint-intro-block rule="no-clr-modal"></app-eslint-intro-block>
+
+    <p>
+      For more information on how to use modal, please refer to the
+      <a target="_blank" href="https://v4.clarity.design/documentation/modals">Clarity Angular Modal</a> or
+      <a target="_blank" href="https://clarity.design/core-components/modal/api/">Clarity Core Modal</a> documentation.
+    </p>
+
+    <demo [tabs]="demo1">
+      <h3>Basic</h3>
+    </demo>
+
+    <demo [tabs]="demo2">
+      <h3>Closable</h3>
+
+      <p>
+        In some cases there is a need to prevent the user from closing the modal. To achieve this there is an option for removing the closable action button
+        on the top right corner of the dialog.
+      </p>
+    </demo>
   `,
 })
-export class ModalPage {}
+export class ModalPage {
+  demo1: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'modal/modal.1.angular.txt',
+      },
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'modal/modal.1.core.txt',
+      },
+      template: 'core',
+    },
+  ];
+
+  demo2: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'modal/modal.2.angular.txt',
+      },
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'modal/modal.2.core.txt',
+      },
+      template: 'core',
+    },
+  ];
+}

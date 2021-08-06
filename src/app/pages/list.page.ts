c6789372e0285234d2d88fcf1043d8eae309096c
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-08-06 12:38:55 +0300

    feat(adoption:list): add list demos
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- src/app/pages/list.page.ts
+++ src/app/pages/list.page.ts
@@ -5,12 +5,81 @@
  */
 
 import { Component } from '@angular/core';
+import { DemoTabData } from '../components/demo.component';
 
 @Component({
   selector: 'app-list',
   template: `
     <h1>List</h1>
-    <not-ready name="List"></not-ready>
+
+    <demo [tabs]="demo">
+      <h3>Ordered list</h3>
+    </demo>
+
+    <demo [tabs]="demo2">
+      <h3>Unordered list</h3>
+    </demo>
+
+    <demo [tabs]="demo3">
+      <h3>Unstyled</h3>
+    </demo>
   `,
 })
-export class ListPage {}
+export class ListPage {
+  demo: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'list/list.1.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'list/list.1.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo2: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'list/list.2.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'list/list.2.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+
+  demo3: DemoTabData[] = [
+    {
+      name: 'Angular',
+      files: {
+        'src/app/app.component.ts': 'list/list.3.angular.txt',
+      },
+      language: 'ts',
+      template: 'angular',
+    },
+    {
+      name: 'Core',
+      files: {
+        'src/app/app.component.ts': 'list/list.3.core.txt',
+      },
+      language: 'ts',
+      template: 'core',
+    },
+  ];
+}

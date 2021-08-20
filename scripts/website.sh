a5777a5f79e1b814b1ee27c4c40845a68aedf183
Jeremy Wilken <gnomation@gnomeontherun.com>
2021-08-20 09:58:35 -0500

    chore(website): setting up website for versioning
    * Remove all of the unneeded routes
    * Simplify GitHub workflows
    
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -4,5 +4,8 @@
 curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
+# Move dev app into website
+mv -f ./dist/dev ./dist/website/dev
+
 # Deploy a preview that can be promoted to v4.clarity.design when we are ready
-node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "v4.clarity.design"
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --message="$GITHUB_REF@$GITHUB_SHA"

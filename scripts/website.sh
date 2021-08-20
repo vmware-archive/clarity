f8be90a65a75f65b5178a4bedede53377b4ca9fb
Jeremy Wilken <gnomation@gnomeontherun.com>
2021-08-20 10:00:11 -0500

    chore(website): update website to host just documentation
    * Remove unneeded routes from website
    * Added docs directory
    * Added license info
    * Updated build as appropriate
    
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -4,5 +4,8 @@
 curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
 # dotenv loads up the env variables into the shell, then deploys through Netlify
 
+# Move dev app into website
+mv -f ./dist/dev ./dist/website/dev
+
 # Deploy a preview that can be promoted to production when we are ready
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --functions dist/netlify --message="$GITHUB_REF@$GITHUB_SHA"

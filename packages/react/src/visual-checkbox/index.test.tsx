1f5e0fc5bcd459b66ec68096f09297fc253bf737
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-08-23 17:35:16 +0300

    feat(react:visual-checkbox): adding visual checkbox react wrapper
    Closes #6248
    
    With this PR we are also fixing our dev environment where we check
    whether the react wrappers are working as expected or not
    
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

---
+++ packages/react/src/visual-checkbox/index.test.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { mount, shallow } from 'enzyme';
+import { CdsInternalVisualCheckbox } from './index';
+
+describe('CdsInternalVisualCheckbox', () => {
+  it('renders', () => {
+    const wrapper = shallow(
+      <div>
+        <CdsInternalVisualCheckbox />
+      </div>
+    );
+    const renderedComponent = wrapper.find('CdsInternalVisualCheckbox');
+    expect(renderedComponent).toBeTruthy();
+  });
+
+  it('snapshot', () => {
+    const wrapper = mount(
+      <div>
+        <CdsInternalVisualCheckbox />
+      </div>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});

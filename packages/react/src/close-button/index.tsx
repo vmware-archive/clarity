377eecb27e82c28238c7fd25ef30f97084f3497c
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-08-25 16:11:55 +0300

    feat(react:close-button): internal close button wrapper
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

---
+++ packages/react/src/close-button/index.tsx
@@ -0,0 +1,4 @@
+import { CdsInternalCloseButton as CloseButton } from '@cds/core/internal-components/close-button';
+import { createComponent } from '../converter/react-wrapper.js';
+
+export const CdsInternalCloseButton = createComponent('cds-internal-close-button', CloseButton);

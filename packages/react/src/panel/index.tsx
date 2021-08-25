f625e5bf8257d622a54b8fb6080b70fd9deab60c
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-08-25 16:29:59 +0300

    feat(react:panel): panel wrapper
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

---
+++ packages/react/src/panel/index.tsx
@@ -0,0 +1,4 @@
+import { CdsInternalPanel as Panel } from '@cds/core/internal-components/panel';
+import { createComponent } from '../converter/react-wrapper.js';
+
+export const CdsInternalPanel = createComponent('cds-internal-panel', Panel);

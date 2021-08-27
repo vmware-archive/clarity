dbacbb9e002df2594c900e1cc32c73e134a0bb6e
Jeremy Wilken <gnomation@gnomeontherun.com>
2021-08-27 10:17:51 -0500

    build: make netlify auto-deploy to production on merge to v5
    Also stop the job from running from any forks, so we don't get duplicates.
    
    Signed-off-by: Jeremy Wilken <gnomation@gnomeontherun.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -7,5 +7,8 @@ curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o 
 # Move dev app into website
 mv -f ./dist/dev ./dist/website/dev
 
-# Deploy a preview that can be promoted to production when we are ready
+[ "$GITHUB_REF" == "refs/head/v5" ] && 
+# Deploy to production
+node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --functions dist/netlify --message="$GITHUB_REF@$GITHUB_SHA" --prod || 
+# Deploy a preview
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --dir=./dist/website --functions dist/netlify --message="$GITHUB_REF@$GITHUB_SHA"

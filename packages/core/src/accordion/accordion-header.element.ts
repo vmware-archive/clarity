38c994bd503140e4fc64df7fdb800080a2005bff
Matt Hippely <mhippely@vmware.com>
2021-09-20 09:18:52 -0700

    fix(core:typography): fix override issues with accordion header and content
    Content projected into accordion-header was being hard coded to 'secondary'.
    The API provided to override it was not being applied. This change removes the specificity
    to span content as the template no longer has a span. It defaults the exposed font custom
    properties to 'secondary' font-weight and font-size.
    
    Content projected into accordion-content now inherits typography from the global scope.
    
    closes #5915, closes #6347
    
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- packages/core/src/accordion/accordion-header.element.ts
+++ packages/core/src/accordion/accordion-header.element.ts
@@ -53,7 +53,7 @@ export class CdsAccordionHeader extends LitElement {
   render() {
     return html`<div class="private-host" cds-layout="horizontal gap:md align:vertical-center wrap:none">
       <cds-icon class="accordion-angle" shape="angle" size="9" inner-offset="2"></cds-icon>
-      <div cds-text="secondary" cds-layout="align:stretch"><slot></slot></div>
+      <div cds-layout="align:stretch"><slot></slot></div>
     </div>`;
   }
 

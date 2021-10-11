3210454dec81c01ce2ef8cb7be49a4a4f4725c3a
Ivan Donchev <idonchev@vmware.com>
2021-10-11 17:08:23 +0300

    chore: add release specific helper scripts
    Signed-off-by: Ivan Donchev <idonchev@vmware.com>

---
+++ scripts/publish-verify.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+'use strict';
+
+// Sanity check verification for the dist folder and modules inside it
+
+const fs = require('fs');
+const path = require('path');
+const pkg = require('../package.json');
+
+const roots = ['clr-angular', 'clr-icons', 'clr-ui'];
+
+function getFile(root) {
+  return `dist/${root}/package.json`;
+}
+
+function verifyRoot(root) {
+  let files = fs.readdirSync(`dist/${root}`);
+  if (!root) {
+    throw `Folder "${root}" could not be loaded.`;
+  }
+  if (files.length <= 1) {
+    throw `Folder "${root}" does not contain enough files.`;
+  }
+  console.log(`* Folder "${root}" OK`);
+}
+
+function verifyModule(file) {
+  file = path.join(process.cwd(), file);
+  const json = require(file);
+  if (!json.version || json.version !== pkg.version) {
+    throw `Versions mismatch at ${file})`;
+  }
+  console.log(`* Module "${file}" OK`);
+}
+
+console.log(`Verifying ${pkg.version} version`);
+roots.forEach(root => {
+  verifyRoot(root);
+  verifyModule(getFile(root));
+});
+console.log('Verification successful.');

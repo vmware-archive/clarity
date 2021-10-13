0a060540068b17630850f3ec2e2fd7a2365b22d4
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-10-13 17:37:54 +0300

    feat(eslint-plugin-clarity-adoption): add button fixer
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- packages/eslint-plugin-clarity-adoption/src/rules/no-clr-button/index.ts
+++ packages/eslint-plugin-clarity-adoption/src/rules/no-clr-button/index.ts
@@ -2,6 +2,7 @@ import { ESLintUtils, TSESTree } from '@typescript-eslint/experimental-utils';
 import { HTMLElement } from '../../types/index';
 import { primaryDisallowedClass, additionalDisallowedClasses, disallowedButtonsSelector } from './disallowed-classes';
 import { lintDecoratorTemplate } from '../decorator-template-helper';
+import { getDeprecatedClassFixes, getTagFixes } from '../html-fixer-helpers';
 
 export const createESLintRule = ESLintUtils.RuleCreator(() => ``);
 export type MessageIds = 'clrButtonFailure';
@@ -9,6 +10,29 @@ export type MessageIds = 'clrButtonFailure';
 function hasDisallowedClasses(classes: Array<string>): boolean {
   return classes.includes(primaryDisallowedClass) && additionalDisallowedClasses.some(cls => classes.includes(cls));
 }
+
+const deprecatedClassToAttributeMap = {
+  btn: '', // remove extraneous badge classes
+
+  'btn-primary': '',
+  'btn-info': '',
+  'btn-secondary': 'action="outline"',
+  'btn-secondary-outline': 'action="outline"',
+  'btn-outline-secondary': 'action="outline"',
+  'btn-success': 'status="success"',
+  'btn-outline': 'action="outline"',
+  'btn-warning': 'status="warning"',
+  'btn-danger': 'status="danger"',
+  'btn-info-outline': 'action="outline"',
+  'btn-success-outline': 'action="outline" status="success"',
+  'btn-danger-outline': 'action="outline" status="danger"',
+  'btn-warning-outline': 'action="outline" status="warning"',
+  'btn-block': 'block',
+  'btn-link': 'action="flat"',
+  'btn-sm': 'size="sm"',
+  'btn-inverse': 'status="inverse"',
+  'btn-icon': '',
+};
 
 export default createESLintRule({
   name: 'no-clr-button',
@@ -31,13 +55,31 @@ export default createESLintRule({
       'HTMLElement[tagName="button"]'(node: HTMLElement): void {
         const classNode = node.attributes?.find(attribute => attribute.attributeName.value === 'class');
         const classes = classNode?.attributeValue?.value?.split(' ') || [];
-        if (hasDisallowedClasses(classes)) {
+        const hasLoader = node.attributes?.find(attribute => attribute.attributeName.value === '[clrLoading]');
+
+        if (hasLoader) {
           context.report({
             node: node as any,
             messageId: 'clrButtonFailure',
           });
+
+          return;
+        }
+
+        if (hasDisallowedClasses(classes)) {
+          context.report({
+            node: node as any,
+            messageId: 'clrButtonFailure',
+            fix: fixer => {
+              const tagFixes = getTagFixes(fixer, node, 'button', 'cds-button');
+              const attributeFixes = getDeprecatedClassFixes(fixer, classNode, deprecatedClassToAttributeMap);
+
+              return [...tagFixes, ...attributeFixes];
+            },
+          });
         }
       },
+
       'ClassDeclaration > Decorator'(node: TSESTree.Decorator): void {
         lintDecoratorTemplate(context, node, disallowedButtonsSelector, 'clrButtonFailure');
       },

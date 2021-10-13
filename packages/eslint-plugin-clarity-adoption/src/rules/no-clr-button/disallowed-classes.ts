d05d796845bcd031c2b285c5bc9120f6fa033fab
Bogdan Bogdanov <bbogdanov@vmware.com>
2021-10-13 17:37:54 +0300

    feat(eslint-plugin-clarity-adoption): add button fixer
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- packages/eslint-plugin-clarity-adoption/src/rules/no-clr-button/disallowed-classes.ts
+++ packages/eslint-plugin-clarity-adoption/src/rules/no-clr-button/disallowed-classes.ts
@@ -1,6 +1,25 @@
 export const primaryDisallowedClass = 'btn';
 
-export const additionalDisallowedClasses = ['btn-primary', 'btn-success', 'btn-warning', 'btn-danger'];
+export const additionalDisallowedClasses = [
+  'btn-info',
+  'btn-success',
+  'btn-warning',
+  'btn-danger',
+  'btn-primary',
+  'btn-outline',
+  'btn-info-outline',
+  'btn-success-outline',
+  'btn-danger-outline',
+  'btn-warning-outline',
+  'btn-secondary',
+  'btn-secondary-outline',
+  'btn-outline-secondary',
+  'btn-block',
+  'btn-inverse',
+  'btn-icon',
+  'btn-link',
+  'btn-sm',
+];
 
 export const disallowedButtonsSelector = additionalDisallowedClasses.map(
   cls => `button.${primaryDisallowedClass}.${cls}`
--- packages/eslint-plugin-clarity-adoption/src/rules/no-clr-button/no-clr-button.spec.ts
+++ packages/eslint-plugin-clarity-adoption/src/rules/no-clr-button/no-clr-button.spec.ts
@@ -10,35 +10,54 @@ htmlRuleTester.run('no-clr-button', rule, {
   invalid: [
     getInvalidButtonTest({
       code: `
-      <button class="btn btn-primary">Shalqlq</button>
+      <button class="btn btn-primary foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo">Shalqlq</cds-button>
       `,
       locations: [{ line: 2, column: 7 }],
     }),
     getInvalidButtonTest({
       code: `
-      <div>
-        <button class="btn btn-primary">Shalqlq</button>
+        <div>
+          <button class="btn btn-primary">Shalqlq</button>
         </div>
       `,
-      locations: [{ line: 3, column: 9 }],
+      output: `
+        <div>
+          <cds-button>Shalqlq</cds-button>
+        </div>
+      `,
+      locations: [{ line: 3, column: 11 }],
     }),
     getInvalidButtonTest({
       code: `
       <div></div>
       <button class="btn btn-primary">Shalqlq</button>
       `,
-      locations: [{ line: 3, column: 7 }],
-    }),
-    getInvalidButtonTest({
-      code: `
+      output: `
       <div></div>
-      <button id="#button" class="btn btn-primary">Shalqlq</button>
+      <cds-button>Shalqlq</cds-button>
       `,
       locations: [{ line: 3, column: 7 }],
     }),
     getInvalidButtonTest({
       code: `
-      <div><ul></ul><button class="btn btn-primary"></button></div>
+      <div></div>
+      <button id="#button" data-id="foo" class="btn btn-primary">Shalqlq</button>
+      `,
+      output: `
+      <div></div>
+      <cds-button id="#button" data-id="foo">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 3, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <div><ul></ul><button class="btn btn-primary">foo</button></div>
+      `,
+      output: `
+      <div><ul></ul><cds-button>foo</cds-button></div>
       `,
       locations: [{ line: 2, column: 21 }],
     }),
@@ -46,6 +65,181 @@ htmlRuleTester.run('no-clr-button', rule, {
       code: `
       <button class="btn btn-primary">Le button</button>
       <div><ul></ul><button class="btn btn-success"></button></div>
+      `,
+      output: `
+      <cds-button>Le button</cds-button>
+      <div><ul></ul><cds-button status="success"></cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-success">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline">Le button</cds-button>
+      <div><ul></ul><cds-button status="success">foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-success-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-success">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline" status="success">Le button</cds-button>
+      <div><ul></ul><cds-button status="success">foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-success-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-block">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline" status="success">Le button</cds-button>
+      <div><ul></ul><cds-button block>foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-success-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-link">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline" status="success">Le button</cds-button>
+      <div><ul></ul><cds-button action="flat">foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-success-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-link btn-sm">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline" status="success">Le button</cds-button>
+      <div><ul></ul><cds-button action="flat" size="sm">foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-success-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-link btn-sm btn-inverse">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline" status="success">Le button</cds-button>
+      <div><ul></ul><cds-button action="flat" size="sm" status="inverse">foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-success-outline">Le button</button>
+      <div><ul></ul><button class="btn btn-icon">foo</button></div>
+      `,
+      output: `
+      <cds-button action="outline" status="success">Le button</cds-button>
+      <div><ul></ul><cds-button>foo</cds-button></div>
+      `,
+      locations: [
+        { line: 2, column: 7 },
+        { line: 3, column: 21 },
+      ],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-danger foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" status="danger">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-warning foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" status="warning">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-info-outline foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" action="outline">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-danger-outline foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" action="outline" status="danger">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-warning-outline foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" action="outline" status="warning">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-secondary-outline foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" action="outline">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button class="btn btn-outline-secondary foo">Shalqlq</button>
+      `,
+      output: `
+      <cds-button class="foo" action="outline">Shalqlq</cds-button>
+      `,
+      locations: [{ line: 2, column: 7 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      <button [clrLoading]="1" class="btn btn-primary">Le button</button>
+      <div><ul></ul><button [clrLoading]="1" class="btn btn-icon">foo</button></div>
       `,
       locations: [
         { line: 2, column: 7 },
@@ -65,6 +259,291 @@ tsRuleTester.run('no-clr-button', rule, {
         template: \`
           <div>
             <button class="btn btn-primary custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-outline custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-success custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-info-outline custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-success-outline custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-danger-outline custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-warning-outline custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-block custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-link custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-sm custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-inverse custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-icon custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-secondary custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-outline-secondary custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-secondary-outline custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-outline-secondary custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-warning custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-primary custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button class="btn btn-primary custom-class">Primary</button>
+          </div>
+        \`
+      })
+      export class CustomButtonComponent {
+      }
+      `,
+      locations: [{ line: 6, column: 13 }],
+    }),
+    getInvalidButtonTest({
+      code: `
+      @Component({
+        selector: 'app-custom-button',
+        template: \`
+          <div>
+            <button [clrLoading]="1" class="btn btn-primary custom-class">Primary</button>
           </div>
         \`
       })

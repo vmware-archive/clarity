c9989e1d8bb17132dc99479f02a39faa39c99e70
Matt Hippely <mhippely@vmware.com>
2021-11-11 16:35:21 -0800

    feat: cds-audio stubs
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

---
+++ packages/core/src/audio/audio.element.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import { LitElement } from 'lit';
+import { property } from '@cds/core/internal';
+import { ClarityAudio } from './audio.service';
+
+export class CdsAudio extends LitElement {
+  /**
+   * Obviously there is a bunch wrong with this pseudo code, but this is the idea
+   * we have a global registry of predefined functions
+   * the ClarityAudio Service can call the correct function when called upon
+   */
+
+  // events could also be a list of events to listen for: click mouseover, custom ...
+  @property({ type: String })
+  events: string;
+
+  @property({ type: String })
+  sound: string;
+
+  constructor() {
+    super();
+    this.addEventListener(this.events, this.play.bind(this));
+  }
+
+  private play = () => {
+    ClarityAudio.playSound(this.sound);
+    console.log('I am playing sound!');
+  };
+  // events property is an event or list of events that notify the system to play a sound
+  // sound property is the sound to play when triggered
+  //
+  // idea: manage the appropriate event listeners for this element instance
+  // idea: pull in the CdsAudioService singleton and tell it play the sound when an event is heard
+}

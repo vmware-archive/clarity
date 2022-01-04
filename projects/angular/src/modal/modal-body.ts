421a3d0df848d24d86a13580aeac1919ddd93645
Steve Haar <info@stevehaar.com>
2022-01-04 13:03:14 -0600

    fix: only focus on modal body if scrollable
    Signed-off-by: Steve Haar <info@stevehaar.com>

--- projects/angular/src/modal/modal-body.ts
+++ projects/angular/src/modal/modal-body.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -17,19 +17,19 @@ export class ClrModalBody implements OnDestroy {
   private tabindex = '0';
   private unlisteners: VoidFunction[] = [];
 
-  constructor(ngZone: NgZone, renderer: Renderer2, host: ElementRef<HTMLElement>) {
-    renderer.setAttribute(host.nativeElement, 'tabindex', this.tabindex);
-
+  constructor(private readonly renderer: Renderer2, private readonly host: ElementRef<HTMLElement>, ngZone: NgZone) {
     ngZone.runOutsideAngular(() => {
+      new ResizeObserver(() => this.addOrRemoveTabIndex()).observe(this.host.nativeElement);
+
       this.unlisteners.push(
-        renderer.listen(host.nativeElement, 'mouseup', () => {
+        this.renderer.listen(this.host.nativeElement, 'mouseup', () => {
           // set the tabindex binding back when click is completed with mouseup
-          renderer.setAttribute(host.nativeElement, 'tabindex', this.tabindex);
+          this.addOrRemoveTabIndex();
         }),
-        renderer.listen(host.nativeElement, 'mousedown', () => {
+        this.renderer.listen(this.host.nativeElement, 'mousedown', () => {
           // tabindex = 0 binding should be removed
           // so it won't be focused when click starts with mousedown
-          renderer.removeAttribute(host.nativeElement, 'tabindex');
+          this.removeTabIndex();
         })
       );
     });
@@ -38,6 +38,24 @@ export class ClrModalBody implements OnDestroy {
   ngOnDestroy(): void {
     while (this.unlisteners.length) {
       this.unlisteners.pop()();
+    }
+  }
+
+  private addTabIndex() {
+    this.renderer.setAttribute(this.host.nativeElement, 'tabindex', this.tabindex);
+  }
+
+  private removeTabIndex() {
+    this.renderer.removeAttribute(this.host.nativeElement, 'tabindex');
+  }
+
+  private addOrRemoveTabIndex() {
+    const modalBody = this.host.nativeElement.parentElement;
+
+    if (modalBody.clientHeight < modalBody.scrollHeight) {
+      this.addTabIndex();
+    } else {
+      this.removeTabIndex();
     }
   }
 }

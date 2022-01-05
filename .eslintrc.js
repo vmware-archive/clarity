88e16c3d498672e3ecee5998c42ab6d03a7ec9a7
Bogdan Bogdanov <bbogdanov@vmware.com>
2022-01-05 16:20:13 +0200

    chore: update ESLint version
    Signed-off-by: Bogdan Bogdanov <bbogdanov@vmware.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',

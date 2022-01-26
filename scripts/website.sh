095a58f6fe3732bbcef7c25832dfabb65e634523
Matt Hippely <mhippely@vmware.com>
2022-01-26 11:54:17 -0800

    build: remove curl script and add github secret for NETLIFY_AUTH_TOKEN
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# This downloads a set of key-value env variables, but blocks any requests that aren't from GitHub Actions
-curl -g https://us-central1-clarity-design-system.cloudfunctions.net/actions -o .env
-# dotenv loads up the env variables into the shell, then deploys through Netlify
-
 # Move dev app into website
 mv -f ./dist/dev ./dist/website/dev
 
-[ "$GITHUB_REF" == "refs/head/angular" ] && 
+[ "$GITHUB_REF" == "refs/head/angular" ] &&
 # Deploy to production
 node -r dotenv/config -- ./node_modules/.bin/netlify deploy --json --dir=./dist/website --message="$GITHUB_REF@$GITHUB_SHA" --prod ||
 # Deploy a preview

2f8da6cf38561a7f822732cc3e84a462d72bfe83
Steve Haar <info@stevehaar.com>
2022-02-01 16:41:55 -0600

    chore: use correct import path in combobox demo

--- src/app/combobox/combobox.demo.ts
+++ src/app/combobox/combobox.demo.ts
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from '@angular/core';
 import { timer, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { tap } from 'rxjs/internal/operators';
+import { map, tap } from 'rxjs/operators';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({

6ddc0c2f895f4837aaec8e8f7fcadf38574c948a
Matt Hippely <mhippely@vmware.com>
2022-02-01 11:08:58 -0800

    docs: fix vue component for parsing core api key/values
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- scripts/website.sh
+++ scripts/website.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Make sre the environment is ready before the deploy command.
+sleep 5
+
 echo "Deploy a preview that can be promoted to clarity.design when we are ready"
 # Deploy a preview that can be promoted to production when we are ready
 ./node_modules/.bin/netlify deploy --json --dir=./dist/website --message="Website - $GITHUB_REF@$GITHUB_SHA" --site "clarity.design"

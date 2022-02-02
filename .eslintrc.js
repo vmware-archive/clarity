4d25be4693b0c4b98da247e9e46adbab740ea761
Steve Haar <info@stevehaar.com>
2022-02-01 18:36:32 -0600

    chore: use string states for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,3 @@
-const OFF = 0;
-const WARN = 1;
-const ERROR = 2;
-
 module.exports = {
   env: {
     browser: true,
@@ -13,14 +9,14 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js', 'postcss.config.js', 'webpack.config.js'],
   plugins: ['@typescript-eslint', 'jasmine', 'unused-imports'],
   rules: {
-    '@typescript-eslint/no-explicit-any': OFF,
-    '@typescript-eslint/no-unused-vars': ERROR,
-    '@typescript-eslint/no-var-requires': ERROR,
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
     curly: 'error',
     eqeqeq: 'error',
-    'jasmine/no-focused-tests': ERROR,
-    'no-irregular-whitespace': [ERROR, { skipTemplates: true }],
+    'jasmine/no-focused-tests': 'error',
+    'no-irregular-whitespace': ['error', { skipTemplates: true }],
     'no-var': 'error',
-    'unused-imports/no-unused-imports-ts': ERROR,
+    'unused-imports/no-unused-imports-ts': 'error',
   },
 };

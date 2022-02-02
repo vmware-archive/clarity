8e5929a307136488c5b8d97fda9970be809080a3
Steve Haar <info@stevehaar.com>
2022-02-01 18:21:50 -0600

    chore: remove explcit eslint settings that match default

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,6 @@ module.exports = {
     jasmine: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
   ignorePatterns: ['.eslintrc.js', 'postcss.config.js', 'webpack.config.js'],
   plugins: ['@typescript-eslint', 'jasmine', 'unused-imports'],
   rules: {
@@ -23,12 +17,8 @@ module.exports = {
     eqeqeq: 'error',
     'no-var': 'error',
     '@typescript-eslint/no-explicit-any': OFF, // Would LOVE to turn this on
-    '@typescript-eslint/explicit-function-return-type': OFF, // TOO MUCH WORK AT THE MOMENT
-    '@typescript-eslint/explicit-module-boundary-types': OFF, // SAME AS ABOVE; Errors on the same issues
-    '@typescript-eslint/no-use-before-define': OFF, // Lots of complaints in tests.
     '@typescript-eslint/no-unused-vars': ERROR, // TypeScript is catching this
     '@typescript-eslint/no-var-requires': ERROR, // Using raw-loader in Storybook
-    '@typescript-eslint/ban-ts-comment': ERROR, // There are some quirks where we do want to use ts-ignore, but should be rare
     'unused-imports/no-unused-imports-ts': ERROR, // It's not covered by default checks; needs external plug-in
     'jasmine/no-focused-tests': ERROR, // Prevent focused tests
     'no-irregular-whitespace': [ERROR, { skipTemplates: true }], // Turn of whitespace checking inside of `` templates

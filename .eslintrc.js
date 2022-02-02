c240083f52d6a36d586c66d841feb7d02623b224
Steve Haar <info@stevehaar.com>
2022-02-01 18:32:58 -0600

    chore: reorder lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,14 +13,14 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js', 'postcss.config.js', 'webpack.config.js'],
   plugins: ['@typescript-eslint', 'jasmine', 'unused-imports'],
   rules: {
-    curly: 'error',
-    eqeqeq: 'error',
-    'no-var': 'error',
     '@typescript-eslint/no-explicit-any': OFF, // Would LOVE to turn this on
     '@typescript-eslint/no-unused-vars': ERROR, // TypeScript is catching this
     '@typescript-eslint/no-var-requires': ERROR, // Using raw-loader in Storybook
-    'unused-imports/no-unused-imports-ts': ERROR, // It's not covered by default checks; needs external plug-in
+    curly: 'error',
+    eqeqeq: 'error',
     'jasmine/no-focused-tests': ERROR, // Prevent focused tests
     'no-irregular-whitespace': [ERROR, { skipTemplates: true }], // Turn of whitespace checking inside of `` templates
+    'no-var': 'error',
+    'unused-imports/no-unused-imports-ts': ERROR, // It's not covered by default checks; needs external plug-in
   },
 };

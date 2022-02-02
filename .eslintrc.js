f39b9f215abaee93e1f128a22b331ff6866fa2ab
Steve Haar <info@stevehaar.com>
2022-02-01 18:33:30 -0600

    chore: remove eslintrc comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,14 +13,14 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js', 'postcss.config.js', 'webpack.config.js'],
   plugins: ['@typescript-eslint', 'jasmine', 'unused-imports'],
   rules: {
-    '@typescript-eslint/no-explicit-any': OFF, // Would LOVE to turn this on
-    '@typescript-eslint/no-unused-vars': ERROR, // TypeScript is catching this
-    '@typescript-eslint/no-var-requires': ERROR, // Using raw-loader in Storybook
+    '@typescript-eslint/no-explicit-any': OFF,
+    '@typescript-eslint/no-unused-vars': ERROR,
+    '@typescript-eslint/no-var-requires': ERROR,
     curly: 'error',
     eqeqeq: 'error',
-    'jasmine/no-focused-tests': ERROR, // Prevent focused tests
-    'no-irregular-whitespace': [ERROR, { skipTemplates: true }], // Turn of whitespace checking inside of `` templates
+    'jasmine/no-focused-tests': ERROR,
+    'no-irregular-whitespace': [ERROR, { skipTemplates: true }],
     'no-var': 'error',
-    'unused-imports/no-unused-imports-ts': ERROR, // It's not covered by default checks; needs external plug-in
+    'unused-imports/no-unused-imports-ts': ERROR,
   },
 };

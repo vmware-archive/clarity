4674793a4f5b85e43c167c3c3cfc1a6da94ecabf
Ashley Ryan <asryan@vmware.com>
2022-02-08 13:15:10 -0500

    docs: add screen reader labels to badge examples
    Signed-off-by: Ashley Ryan <asryan@vmware.com>

--- projects/website/src/app/documentation/demos/badges/badge-statuses.ts
+++ projects/website/src/app/documentation/demos/badges/badge-statuses.ts
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2016-2020 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from '@angular/core';
 
 const HTML_EXAMPLE = `
-<span class="badge badge-info">2</span>
-<span class="badge badge-success">3</span>
-<span class="badge badge-warning">12</span>
-<span class="badge badge-danger">15</span>
+<span class="badge badge-info">2 <span class="clr-sr-only">items in an info badge</span></span>
+<span class="badge badge-success">3 <span class="clr-sr-only">items in a sucess badge</span></span>
+<span class="badge badge-warning">12 <span class="clr-sr-only">items in a warning badge</span></span>
+<span class="badge badge-danger">15 <span class="clr-sr-only">items in a danger badge</span></span>
 `;
 
 @Component({

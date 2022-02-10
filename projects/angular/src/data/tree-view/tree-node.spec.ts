1f61d0271c26ac6410fd45b5775be8a3396f6d1e
Scott Mathis <smathis@vmware.com>
2022-02-10 09:15:46 -0500

    fix(tree-view): remove duplicate aria roll from treenode
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- projects/angular/src/data/tree-view/tree-node.spec.ts
+++ projects/angular/src/data/tree-view/tree-node.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -338,7 +338,8 @@ export default function (): void {
         contentContainer = this.clarityElement.querySelector('.clr-tree-node-content-container');
       });
 
-      it('adds role treeitem to node content container by default', function (this: Context) {
+      it('adds role treeitem to focusable div that contains node content', function (this: Context) {
+        expect(this.clarityElement.hasAttribute('role')).toBe(false, 'no duplicate treeitem role on parent');
         expect(contentContainer.getAttribute('role')).toBe('treeitem');
       });
 

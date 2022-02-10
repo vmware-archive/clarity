8a247b4b03e6aeec59ef6beb9f04b85c03750ff1
Scott Mathis <smathis@vmware.com>
2022-02-10 09:15:46 -0500

    fix(tree-view): remove duplicate aria roll from treenode
    Signed-off-by: Scott Mathis <smathis@vmware.com>

--- projects/angular/src/data/tree-view/tree-node.ts
+++ projects/angular/src/data/tree-view/tree-node.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -59,7 +59,6 @@ const LVIEW_CONTEXT_INDEX = 8;
     ]),
   ],
   host: {
-    '[attr.role]': '"treeitem"',
     '[class.clr-tree-node]': 'true',
   },
 })

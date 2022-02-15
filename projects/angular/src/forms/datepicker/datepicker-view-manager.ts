1bfc11cd80a5297f9359a1e3619f6b6730989817
Ashley Ryan <asryan@vmware.com>
2022-02-15 11:12:40 -0500

    fix(date-picker): add role="modal" to datepicker popup
    VPAT-8802
    
    Signed-off-by: Ashley Ryan <asryan@vmware.com>

--- projects/angular/src/forms/datepicker/datepicker-view-manager.ts
+++ projects/angular/src/forms/datepicker/datepicker-view-manager.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -16,6 +16,7 @@ import { ViewManagerService } from './providers/view-manager.service';
   host: {
     '[class.datepicker]': 'true',
     '[attr.aria-modal]': 'true',
+    role: 'modal',
   },
 })
 export class ClrDatepickerViewManager {

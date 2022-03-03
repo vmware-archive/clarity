edce4ec095577f9bfa4065e753910f28b1e04850
Ashley Ryan <asryan@vmware.com>
2022-03-03 12:40:24 -0500

    fix(tabs): remove unnecessary aria-expanded
    - also remove aria-hidden false on the tab-link
    
    fixes VPAT-707
    
    Signed-off-by: Ashley Ryan <asryan@vmware.com>

--- projects/angular/src/layout/tabs/tab-link.directive.ts
+++ projects/angular/src/layout/tabs/tab-link.directive.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware, Inc. All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware, Inc. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -27,7 +27,6 @@ let nbTabLinkComponents = 0;
 @Directive({
   selector: '[clrTabLink]',
   host: {
-    '[attr.aria-hidden]': 'false',
     '[class.btn]': 'true',
     role: 'tab',
     type: 'button',
